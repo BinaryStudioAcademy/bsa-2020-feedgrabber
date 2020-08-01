e62a719194dbe4e5ea5010540248b3a0d9a63d13
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2020-08-01 21:28:05 +0300

    Updated docker hub images

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ docker build -f .docker/client.Dockerfile -t 'fg-client' .
 
 docker login -u $docker_user -p $docker_pass
 
-docker tag fg-core:latest feedgrabber2020/dockerhub:api
-docker tag fg-client:latest feedgrabber2020/dockerhub:client
+docker tag fg-core:latest feedgrabber2020/dockerhub:fg-core
+docker tag fg-client:latest feedgrabber2020/dockerhub:fg-client
 
 docker push feedgrabber2020/dockerhub:fg-core
 docker push feedgrabber2020/dockerhub:fg-client
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 scp -r ./.docker/nginx $vm_user@$server_ip:~/.docker/nginx
 scp ./.docker/docker-compose.yml $vm_user@$server_ip:~/.docker
 
-ssh $vm_user@$server_ip "sudo docker pull bsa2020knewless/dockerhub:api"
-ssh $vm_user@$server_ip "sudo docker pull bsa2020knewless/dockerhub:client"
+ssh $vm_user@$server_ip "sudo docker pull feedgrabber2020/dockerhub:fg-core"
+ssh $vm_user@$server_ip "sudo docker pull feedgrabber2020/dockerhub:fg-client"
 ssh $vm_user@$server_ip "sudo docker-compose -f ~/.docker/docker-compose.yml up -d"
