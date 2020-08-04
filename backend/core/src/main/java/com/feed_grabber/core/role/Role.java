d15fe060b91612329f18544da028039c3f2d8df6
lenazelenaya <lena.zelenaya@gmail.com>
2020-08-04 13:53:50 +0300

    Add initial models for role and team

---
+++ backend/core/src/main/java/com/feed_grabber/core/role/Role.java
@@ -0,0 +1,24 @@
+package com.feed_grabber.core.role;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+}
