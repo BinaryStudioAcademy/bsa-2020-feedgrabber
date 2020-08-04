8a5ca00920b827b9502bbb2758f1c299ad7fc2af
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-04 20:04:59 +0300

    improve Header style

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import { Menu, Icon, Image } from 'semantic-ui-react';
+import React from "react";
+import { Menu, Icon, Image, Header as HeaderUI, Button, Dropdown } from "semantic-ui-react";
+
+import styles from './styles.module.scss';
 
 const Header = ({ user }) => (
     <Menu secondary>
         <Menu.Item>
-            <img src="https://img.icons8.com/cotton/64/000000/chat.png" alt=""/>
+            <HeaderUI>
+                <Image circular src="https://img.icons8.com/cotton/64/000000/chat.png" />
+                {' '}
+                <span>Feedgrabber</span>
+            </HeaderUI>
         </Menu.Item>
         <Menu.Menu position="right">
-            <Menu.Item >
-                <Icon name="bell" />
+            <Menu.Item>
+                <Button basic icon type="button" className={`${styles.menuBtn} ${styles.logoutBtn}`}>
+                    <Icon name="bell" size="large" />
+                </Button>
             </Menu.Item>
-            <Menu.Item >
-                <Image circular src={user.avatar} />
+            <Menu.Item>
+                <Dropdown
+                    className="icon"
+                    floating
+                    header="user options"
+                    trigger={<Image avatar src="https://img.icons8.com/cotton/64/000000/chat.png" />}>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>User</Dropdown.Item>
+                        <Dropdown.Item>Setting</Dropdown.Item>
+                        <Dropdown.Item>Profile</Dropdown.Item>
+                        <Dropdown.Item>Forms</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </Menu.Item>
-            <Menu.Item >
-                <Icon name="log out" />
+            <Menu.Item>
+                <Button basic icon type="button" className={`${styles.menuBtn} ${styles.logoutBtn}`}>
+                    <Icon name="log out" size="large" />
+                </Button>
             </Menu.Item>
         </Menu.Menu>
     </Menu>
