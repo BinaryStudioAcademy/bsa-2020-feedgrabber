b52bce9ead4fac7c10b485e97de46215f4b1622d
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-04 20:57:22 +0300

    now items of the SideMenu is clickable and redict user to corresponding page

--- frontend/src/components/SideMenu/index.tsx
+++ frontend/src/components/SideMenu/index.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
+import { useHistory } from 'react-router-dom';
 
-const SideMenu = () => (
-    <Menu vertical>
-        <Menu.Item>
-            <Icon name="th"/>
-            Company dashboard
-        </Menu.Item>
-        <Menu.Item>
-            <Icon name="edit"/>
-            Form editor
-        </Menu.Item>
-        <Menu.Item>
-            <Icon name="clock outline"/>
-            Pending feedbacks
-        </Menu.Item>
-        <Menu.Item>
-            <Icon name="bookmark"/>
-            Assign feedbacks
-        </Menu.Item>
-    </Menu>
-);
+const SideMenu = () => {
+    const history = useHistory();
+    const handleCompanyDashboardClick = () => {
+        history.push('/company');
+    }
+    const handleFormEditorClick = () => {
+        history.push('/editor');
+    }
+    const handlePendingClick = () => {
+        history.push('/pending');
+    }
+    const handleAssignClick = () => {
+        history.push('/assign');
+    }
+    return (
+        <Menu vertical>
+            <Menu.Item onClick={handleCompanyDashboardClick}>
+                <Icon name="th"/>
+                Company dashboard
+            </Menu.Item>
+            <Menu.Item onClick={handleFormEditorClick}>
+                <Icon name="edit"/>
+                Form editor
+            </Menu.Item>
+            <Menu.Item onClick={handlePendingClick}>
+                <Icon name="clock outline"/>
+                Pending feedbacks
+            </Menu.Item>
+            <Menu.Item onClick={handleAssignClick}>
+                <Icon name="bookmark"/>
+                Assign feedbacks
+            </Menu.Item>
+        </Menu>
+    );
+};
 
 export default SideMenu;
