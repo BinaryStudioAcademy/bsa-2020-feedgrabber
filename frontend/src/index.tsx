592bd409dd39e50ca782512558cbf3e4050aa730
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2020-08-04 10:30:37 +0300

    Added semantic

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
 import "./styles/common.sass";
+import "semantic-ui-css/semantic.min.css";
 import App from "./containers/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
