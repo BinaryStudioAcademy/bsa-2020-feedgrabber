4ec8133fcee557c821ab9c69e016b808b62c0e84
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-08-04 17:00:34 +0300

    Add company entity with service

---
+++ backend/core/src/main/java/com/feed_grabber/core/company/Company.java
@@ -0,0 +1,43 @@
+package com.feed_grabber.core.company;
+
+import com.feed_grabber.core.company.dto.CompanyDto;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@Table(name = "companies")
+public class Company {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String address;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String corporateEmail;
+
+    public static Company fromDto(CompanyDto companyDto) {
+        return Company.builder()
+                .id(companyDto.getId())
+                .name(companyDto.getName())
+                .address(companyDto.getAddress())
+                .phoneNumber(companyDto.getPhoneNumber())
+                .corporateEmail(companyDto.getEmail())
+                .build();
+    }
+}
