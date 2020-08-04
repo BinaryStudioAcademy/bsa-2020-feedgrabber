9b7c86cd421769e695d3d77d95e90538313717ae
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-04 16:24:15 +0300

    Add user model

--- backend/core/src/main/java/com/feed_grabber/core/auth/model/AuthUser.java
+++ backend/core/src/main/java/com/feed_grabber/core/auth/model/AuthUser.java
@@ -1,13 +1,21 @@
 package com.feed_grabber.core.auth.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.userdetails.User;
 
+
+import java.util.UUID;
 
 import static java.util.Collections.emptyList;
 
 public class AuthUser extends User {
+    @Getter
+    @Setter
+    private UUID id;
 
-    public AuthUser(String username, String password) {
+    public AuthUser(UUID id, String username, String password) {
         super(username, password, emptyList());
+        this.setId(id);
     }
 }
