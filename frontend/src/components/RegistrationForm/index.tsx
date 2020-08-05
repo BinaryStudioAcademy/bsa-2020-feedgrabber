c11349ea2e524047db9e125b019e39a58730857e
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-08-05 13:15:00 +0300

    input validation added

--- frontend/src/components/RegistrationForm/index.tsx
+++ frontend/src/components/RegistrationForm/index.tsx
@@ -10,25 +10,75 @@ const RegistrationPage = ({ register }) => {
     const [companyName, setCompanyName] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleUserInput = (body: string, fieldName: string) => {
-        switch (fieldName) {
-            case "email":
-                setEmail(body);
-                break;
-            case "companyName":
-                setCompanyName(body);
-                break;
-            case "password":
-                setPassword(body);
-                break;
+    const validateEmail = (email: string): boolean => {
+        const emailRegExp = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*' +
+            '@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+
+        const validationMessage = document.getElementById("email-validation");
+        if (emailRegExp.test(email)) {
+            validationMessage.style.display = "none";
+            return true;
+        } else {
+            validationMessage.style.display = "block";
+            return false;
+        }
+    };
+
+    const validatePassword = (password: string): boolean => {
+        const validationMessage = document.getElementById("password-validation");
+
+        if (password.length <= 6) {
+            validationMessage.textContent = "Use 6 characters or more for your password";
+            validationMessage.style.display = "block";
+            return false;
+        }
+
+        if (!password.match(/[a-z]/)) {
+            validationMessage.textContent = "Password must contain at least 1 lower case letter";
+            validationMessage.style.display = "block";
+            return false;
+        }
+
+        if (!password.match(/[A-Z]/)) {
+            validationMessage.textContent = "Password must contain at least 1 upper case letter";
+            validationMessage.style.display = "block";
+            return false;
+        }
+
+        if (!password.match(/[0-9]/g)) {
+            validationMessage.textContent = "Password must contain at least 1 digit";
+            validationMessage.style.display = "block";
+            return false;
+        }
+
+        /* if (!password.match(/[^a-zA-Z\d]/g)) {
+            validationMessage.textContent = "Password must contain at least 1 special symbol";
+            validationMessage.style.display = "block";
+            return false;
+        }*/
+
+        validationMessage.style.display = "none";
+        return true;
+    };
+
+    const validateCompany = (companyName: string): boolean => {
+        const validationMessage = document.getElementById("company-validation");
+
+        if (!companyName) {
+            validationMessage.style.display = "block";
+            return false;
+        } else {
+            validationMessage.style.display = "none";
+            return true;
         }
     };
 
     const onRegistrationSubmit = async() => {
-        const emailRegExp = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*' +
-            '@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+        const isEmailValid = validateEmail(email);
+        const isCompanyValid = validateCompany(companyName);
+        const isPasswordValid = validatePassword(password);
 
-        if (!emailRegExp.test(email.toLowerCase()) || !companyName || !password) {
+        if (!(isEmailValid && isCompanyValid && isPasswordValid)) {
             return;
         }
 
@@ -42,13 +92,31 @@ const RegistrationPage = ({ register }) => {
 
     return (
         <div className={styles.registrationPage}>
-            <div className={styles.registerForm}>
-              <div>Email</div>
-              <input type="text" onChange={e => handleUserInput(e.target.value, "email")}/>
-              <div>Company name</div>
-              <input type="text" onChange={e => handleUserInput(e.target.value, "companyName")}/>
-              <div>Password</div>
-              <input type="password" onChange={e => handleUserInput(e.target.value, "password")}/>
+            <div className={styles.registrationForm}>
+              <div className={styles.registrationField + " email"}>
+                <div>Email</div>
+                <input type="text"
+                       onChange={e => setEmail(e.target.value)}
+                       onBlur={() => validateEmail(email)}
+                />
+                <div id="email-validation" className={styles.validationMessage}>Email not valid</div>
+              </div>
+              <div className={styles.registrationField + " companyName"}>
+                <div>Company name</div>
+                <input type="text"
+                       onChange={e => setCompanyName(e.target.value)}
+                       onBlur={() => validateCompany(companyName)}
+                />
+                <div id="company-validation" className={styles.validationMessage}>Enter company name</div>
+              </div>
+              <div className={styles.registrationField + " password"}>
+                <div>Password</div>
+                <input type="password"
+                       onChange={e => setPassword(e.target.value)}
+                       onBlur={() => validatePassword(password)}
+                />
+                <div id="password-validation" className={styles.validationMessage} />
+              </div>
               <button type="button" onClick={onRegistrationSubmit}>Register</button>
               <p className={styles.message}>Already registered?  <a href="#">Sign In</a></p>
             </div>
