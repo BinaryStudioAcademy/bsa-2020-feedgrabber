b3e350067eb1f17288993b30e42cb2b6afe70d7b
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-05 17:44:11 +0300

    Create creation model

---
+++ backend/core/src/main/java/com/feed_grabber/core/role/dto/RoleCreationDto.java
@@ -0,0 +1,20 @@
+package com.feed_grabber.core.role.dto;
+
+import com.feed_grabber.core.role.SystemRole;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class RoleCreationDto {
+
+    private String name;
+
+    private String description;
+
+  private SystemRole systemRole;
+
+    private UUID company;
+}
