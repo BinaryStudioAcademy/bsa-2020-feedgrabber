e5cfad3ac23f89fddcad05f936ac9a34c9cccec1
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-08-05 14:49:42 +0300

    Add mapping for company entity

--- backend/core/src/main/java/com/feed_grabber/core/company/dto/CompanyDto.java
+++ backend/core/src/main/java/com/feed_grabber/core/company/dto/CompanyDto.java
@@ -3,25 +3,16 @@ package com.feed_grabber.core.company.dto;
 import com.feed_grabber.core.company.Company;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class CompanyDto {
     private UUID id;
     private String name;
     private String address;
     private String phoneNumber;
     private String email;
-
-    public static CompanyDto fromEntity(Company company) {
-        return CompanyDto.builder()
-                .id(company.getId())
-                .name(company.getName())
-                .address(company.getAddress())
-                .phoneNumber(company.getPhoneNumber())
-                .email(company.getCorporateEmail())
-                .build();
-    }
 }
