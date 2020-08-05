4b2354e41088342cedef26ad0db350f9c1b39155
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-08-05 15:11:17 +0300

    Add mapping for user entity

--- backend/core/src/main/java/com/feed_grabber/core/user/dto/UserDto.java
+++ backend/core/src/main/java/com/feed_grabber/core/user/dto/UserDto.java
@@ -3,26 +3,16 @@ package com.feed_grabber.core.user.dto;
 import com.feed_grabber.core.user.model.User;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.util.UUID;
 
 @Data
-@Value
-@Builder
+@NoArgsConstructor
 public class UserDto {
-    UUID id;
-    String email;
-    String username;
-    String password;
-
-    public static UserDto fromEntity(User user) {
-        return UserDto
-                .builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .build();
-    }
+    private UUID id;
+    private String email;
+    private String username;
+    private String password;
 }
