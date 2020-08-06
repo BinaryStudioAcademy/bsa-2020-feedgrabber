8cba42bebdb809b8842833ef47c19ff5fbbbe7c0
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-08-06 12:59:40 +0300

    Update RoleMapper
    Edit annotations in RoleDetailsDto

--- backend/core/src/main/java/com/feed_grabber/core/role/RoleMapper.java
+++ backend/core/src/main/java/com/feed_grabber/core/role/RoleMapper.java
@@ -1,13 +1,18 @@
 package com.feed_grabber.core.role;
 
+import com.feed_grabber.core.role.dto.RoleCreationDto;
+import com.feed_grabber.core.role.dto.RoleDetailsDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface RoleMapper {
     RoleMapper MAPPER = Mappers.getMapper(RoleMapper.class);
 
-    Role roleDtoToModel(RoleDto roleDto);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "companyId", target = "company.id")
+    Role roleDtoToModel(RoleCreationDto roleDto);
 
-    RoleDto roleToRoleDto(Role role);
+    RoleDetailsDto roleToRoleDto(Role role);
 }
