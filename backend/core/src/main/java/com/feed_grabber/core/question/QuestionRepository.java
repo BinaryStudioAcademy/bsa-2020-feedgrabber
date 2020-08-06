aa81501fb9923965a54032783f0f4354dad77d5b
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-08-06 15:23:43 +0300

    Added Question entity and migration

---
+++ backend/core/src/main/java/com/feed_grabber/core/question/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.feed_grabber.core.question;
+
+import com.feed_grabber.core.question.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface QuestionRepository extends JpaRepository<Question, UUID> {
+}
