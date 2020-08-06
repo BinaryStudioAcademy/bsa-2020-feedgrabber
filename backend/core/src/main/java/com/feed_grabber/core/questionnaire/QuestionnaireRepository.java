b05d54034782c7ab82fa4c22d24033e8eeb620a9
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-08-06 14:42:58 +0300

    Questionnaire entity and migration

---
+++ backend/core/src/main/java/com/feed_grabber/core/questionnaire/QuestionnaireRepository.java
@@ -0,0 +1,9 @@
+package com.feed_grabber.core.questionnaire;
+
+import com.feed_grabber.core.questionnaire.model.Questionnaire;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface QuestionnaireRepository extends JpaRepository<Questionnaire, UUID> {
+}
