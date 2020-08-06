b6ff671a79e1703c0cc9f07bfac375a967711162
AlexandrKarpachov <xalexandrx9@gmail.com>
2020-08-06 21:08:05 +0300

    made routing

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as toastr } from 'react-redux-toastr';
-import data from '../screens/Home/reducers';
 import signInReducer from '../containers/SignInBox/reducer';
 
 export default combineReducers({
   toastr,
-  data,
   profile: signInReducer
 });
--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,10 +1,8 @@
 import { all } from 'redux-saga/effects';
-import homeSagas from 'screens/Home/sagas';
 import loginSagas from '../containers/SignInBox/sagas';
 
 export default function* rootSaga() {
   yield all([
-    homeSagas(),
     loginSagas()
   ]);
 }
