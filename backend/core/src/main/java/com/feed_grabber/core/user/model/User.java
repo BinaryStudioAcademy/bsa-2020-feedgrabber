0636ee5a94045e7176b2c9034aec5d7d7187e241
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-06 10:18:13 +0300

    Change relation ship with role

--- backend/core/src/main/java/com/feed_grabber/core/user/model/User.java
+++ backend/core/src/main/java/com/feed_grabber/core/user/model/User.java
@@ -42,7 +42,7 @@ public class User {
     @JoinColumn(name = "team_id")
     private List<Team> teams;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
     private Role role;
 }
