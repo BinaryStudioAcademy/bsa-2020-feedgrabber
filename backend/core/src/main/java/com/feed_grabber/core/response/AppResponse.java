c44e8ad6496a559d2ce47aa3edfa3ffb6065afdb
Olexander <olexandermarchuk@gmail.com>
2020-08-07 11:51:22 +0300

    add constructor

--- backend/core/src/main/java/com/feed_grabber/core/response/AppResponse.java
+++ backend/core/src/main/java/com/feed_grabber/core/response/AppResponse.java
@@ -1,5 +1,7 @@
 package com.feed_grabber.core.response;
 
+import org.springframework.context.ApplicationListener;
+
 public class AppResponse<T> {
     public final T data;
     public final Exception error;
@@ -9,8 +11,14 @@ public class AppResponse<T> {
         this.error = null;
     }
 
+    public AppResponse(Exception error) {
+        this.data = null;
+        this.error = error;
+    }
+
     public AppResponse(T data, Exception error) {
         this.data = data;
         this.error = error;
     }
+
 }
