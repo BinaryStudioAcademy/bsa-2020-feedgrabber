22a76dc96b5595732d8ab2fcefd2fe2195c6435a
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-08-07 10:19:21 +0300

    merge conflicts resolved

--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects';
 import homeSagas from 'screens/Home/sagas';
+import loginSagas from '../containers/SignInBox/sagas';
 import registrationSaga from "components/RegistrationForm/sagas"
 
 export default function* rootSaga() {
   yield all([
     homeSagas(),
+    loginSagas(),
     registrationSaga()
   ]);
 }
