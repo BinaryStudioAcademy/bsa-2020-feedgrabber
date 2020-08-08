0bb8bfed22c31970d8410510dd7854390e5db76b
nick <nick>
2020-08-08 11:26:03 +0300

    sign up form validation

--- frontend/src/components/AuthForm/SignForm/SignUpForm.tsx
+++ frontend/src/components/AuthForm/SignForm/SignUpForm.tsx
@@ -2,11 +2,12 @@ import React, {FC} from 'react';
 import Typography from './Typography';
 import Input from './Input';
 import Button from './Button';
-import {Formik} from "formik";
+import {Form, Formik} from "formik";
 import * as yup from "yup";
 import {registerRoutine} from "../routines";
 import {connect} from "react-redux";
 import {IRegisterData} from "../../../models/auth/types";
+import {Message} from "semantic-ui-react";
 
 interface IRegisterProps {
     signUp: (data: IRegisterData) => void;
@@ -15,25 +16,25 @@ interface IRegisterProps {
 }
 
 const schema = yup.object().shape({
-    // companyName: yup
-    //     .string()
-    //     .required("Required")
-    //     .min(6, "Too Short!")
-    //     .max(20, "Too Long!"),
-    // email: yup
-    //     .string()
-    //     .email()
-    //     .required(),
-    // password: yup
-    //     .string()
-    //     .required("Required")
-    //     .min(6, "Too Short!")
-    //     .max(30, "Too Long!"),
-    // username: yup
-    //     .string()
-    //     .required("Required")
-    //     .min(5, "Too Short!")
-    //     .max(15, "Too Long!")
+    companyName: yup
+        .string()
+        .required("Company name required")
+        .min(6, "Company name too short!")
+        .max(20, "Company name too long!"),
+    email: yup
+        .string()
+        .email("Email must be valid")
+        .required(),
+    password: yup
+        .string()
+        .required("Password required")
+        .min(6, "Password too short!")
+        .max(30, "Password too long!"),
+    username: yup
+        .string()
+        .required("Username required")
+        .min(5, "Username too short!")
+        .max(15, "Username too long!")
 });
 
 const SignUpForm: FC<IRegisterProps> = props => {
@@ -59,33 +60,41 @@ const SignUpForm: FC<IRegisterProps> = props => {
                   values,
                   handleChange,
                   handleBlur,
-                  handleSubmit,
-                  touched
-              }) => (
-                <form className={className} onSubmit={handleSubmit}>
-                    <Typography fontWeight="bold" variant="h4">Create Account</Typography>
-                    <Typography variant="body2">or use your email for registration</Typography>
-                    <Input name="username" placeholder="Username" value={values.username}
-                           error={touched.username && (errors.username ?? null)}
-                           onChange={handleChange}
-                           onBlur={handleBlur}
-                    />
-                    <Input name="email" type="email" placeholder="Email" value={values.email}
-                           error={touched.email && (errors.email ?? null)}
-                           onChange={handleChange} onBlur={handleBlur}
-                    />
-                    <Input name="companyName" placeholder="Company" value={values.companyName}
-                           error={touched.companyName && (errors.companyName ?? null)}
-                           onChange={handleChange} onBlur={handleBlur}
-                    />
-                    <Input name="password" type="password" placeholder="Password" value={values.password}
-                           error={touched.password && (errors.password ?? null)}
-                           onChange={handleChange} onBlur={handleBlur}
-                    />
-                    <Button variant="secondary" type="submit" marginTop="1.17rem">
-                        Sign Up
-                    </Button>
-                </form>)}
+                  handleSubmit
+              }) => {
+                let errorText;
+                for (const key in values)
+                    if (errors[key])
+                        errorText = errors[key];
+
+                return (
+                    <Form className={className} onSubmit={handleSubmit} autoComplete="off">
+                        <Typography fontWeight="bold" variant="h4">Create Account</Typography>
+                        <Typography variant="body2">or use your email for registration</Typography>
+                        <Input id="username" name="username" placeholder="Username" value={values.username}
+                               onChange={handleChange} onBlur={handleBlur}
+                        />
+                        <Input name="email" placeholder="Email" value={values.email}
+                               onChange={handleChange} onBlur={handleBlur}
+                        />
+                        <Input name="companyName" placeholder="Company" value={values.companyName}
+                               onChange={handleChange} onBlur={handleBlur}
+                        />
+                        <Input name="password" type="password" placeholder="Password" value={values.password}
+                               onChange={handleChange} onBlur={handleBlur}
+                        />
+                        {
+                            errorText && <Message attached="top" error size="small" content={errorText}/>
+                        }
+                        <Button disabled={!!errorText}
+                                variant="secondary"
+                                type="submit"
+                                marginTop="1.17rem">
+                            Sign Up
+                        </Button>
+                    </Form>);
+            }
+            }
         </Formik>
     );
 };
