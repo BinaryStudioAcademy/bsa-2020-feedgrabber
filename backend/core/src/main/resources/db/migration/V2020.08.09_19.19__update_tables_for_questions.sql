1584758f2fd332c47fffbd6e96d2dc94b6cbd32a
Ivan Husarov <ivan.husarov@gmail.com>
2020-08-09 21:40:49 +0300

    migration fix

--- backend/core/src/main/resources/db/migration/V2020.08.09_19.19__update_tables_for_questions.sql
+++ backend/core/src/main/resources/db/migration/V2020.08.09_19.19__update_tables_for_questions.sql
@@ -9,6 +9,7 @@ create table questionnaire_question
 
 alter table questions
 drop column if exists questionnaire_id,
+add column payload varchar not null,
 add column type varchar(255) not null,
 add column company_id uuid not null,
 add foreign key (company_id) references companies (id);
