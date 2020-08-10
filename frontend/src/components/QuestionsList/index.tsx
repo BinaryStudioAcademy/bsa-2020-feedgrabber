b627d9eda1b64a6bd3847e1d8020f1d8a34a5862
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-08-10 11:44:51 +0300

    Delete defaultProps

--- frontend/src/components/QuestionsList/index.tsx
+++ frontend/src/components/QuestionsList/index.tsx
@@ -66,29 +66,6 @@ const QuestionsList: FunctionComponent<IQuestionsListProps> = ({ questions, isLo
     );
 };
 
-const defaultProps: IQuestionsListProps= {
-    questions: null,
-    isLoading: true,
-    loadQuestions() {
-        return [
-            {
-                id: "1",
-                category: "Soft skills",
-                type: 'free text',
-                text: "Can you tell me about a time when you successfully led a team through a sticky situation?"
-            },
-            {
-                id: "2",
-                category: "Leadership",
-                type: 'scale',
-                text: "Are you able to delegate responsibilities efficiently?"
-            }
-        ];
-    }
-};
-
-QuestionsList.defaultProps = defaultProps;
-
 const mapStateToProps = rootState => ({
     questions: rootState.questions.questions,
     isLoading: rootState.questions.isLoading
