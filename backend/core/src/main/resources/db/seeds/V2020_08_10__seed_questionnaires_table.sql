0482c06e0bebacd0685e218147b52c12441eabba
nick <nick>
2020-08-10 14:08:21 +0300

    start new migrations

---
+++ backend/core/src/main/resources/db/seeds/V2020_08_10__seed_questionnaires_table.sql
@@ -0,0 +1,8 @@
+insert into questionnaires
+(id, area, name, company_id)
+values
+('953f401c-7ba7-4523-805e-6fee70e2cb14', 'frontend', 'Hipsters', 'a099b732-23ba-49aa-9b94-a9541b1c18b7'),
+('5d538bc8-0644-42de-b1eb-d8afaf47871e', 'backend', 'Geeks', 'b280ecd5-243f-4b55-ba4e-4777ef49453b'),
+('6ec4bcdc-7121-4dcf-b186-616cbb5c488c', 'mobile', 'Hackers', 'a099b732-23ba-49aa-9b94-a9541b1c18b7'),
+('2591bf93-d4e4-4fce-95b6-c730c7ef5fdd', 'backend', 'Nerds', 'b280ecd5-243f-4b55-ba4e-4777ef49453b'),
+('99abe5fc-8a24-4d2e-91b7-af68cb67fb5e', 'desktop', 'Olds', 'b6c3b74f-b7e6-4718-92fc-e9491ee51d0b');
---
+++ backend/core/src/main/resources/db/seeds/V2020_08_10__seed_questions_table.sql
@@ -0,0 +1,8 @@
+insert into questions
+(id, text, category_id, payload, type, company_id)
+values
+('31c429b6-4f13-4f8b-926d-d4ccd6c30ec0', 'Describe your feelings about company', '', '31c6aeb4-3dad-4bbf-a3d1-21069ac67fc7'),
+('7d44b4b0-daf8-11ea-87d0-0242ac130003', 'What do you think about our new office?', '', '31c6aeb4-3dad-4bbf-a3d1-21069ac67fc7'),
+('80f43b08-daf8-11ea-87d0-0242ac130003', 'Are you satisfied by your team?', '', '31c6aeb4-3dad-4bbf-a3d1-21069ac67fc7'),
+('83e43ab6-daf8-11ea-87d0-0242ac130003', 'List top 10 annoying things', '', '31c6aeb4-3dad-4bbf-a3d1-21069ac67fc7'),
+('8997a038-daf8-11ea-87d0-0242ac130003', 'How long you have been working with us?', '', '{},' '31c6aeb4-3dad-4bbf-a3d1-21069ac67fc7');
