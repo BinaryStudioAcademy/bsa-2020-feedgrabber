749a5442fbf31f3f411cbf9254229073a1554862
nick <nick>
2020-08-10 12:16:00 +0300

    add fields to IAppState

--- frontend/src/models/IAppState.ts
+++ frontend/src/models/IAppState.ts
@@ -1,4 +1,6 @@
 import {IUserErrors, IUserInfo} from "./user/types";
+import {IQuestion} from "./IQuesion";
+import {IQuestionnaire} from "./IQuestionnaire";
 
 export interface IAppState {
     toastr: any;
@@ -6,5 +8,14 @@ export interface IAppState {
         isLoading: boolean;
         info: IUserInfo;
         error: IUserErrors;
+    };
+    forms: {
+        questions: IQuestion[];
+        questionnaires: IQuestionnaire[];
+        currentQuestionnaire: {
+            questionnaire: IQuestionnaire;
+            questions: IQuestion[];
+        };
+        currentQuestion: IQuestion;
     };
 }
