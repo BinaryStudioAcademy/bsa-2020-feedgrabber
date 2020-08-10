d3dd27d045648559169daacdc1a66dadac5b2909
nick <nick>
2020-08-10 10:37:21 +0300

    form saga draft

--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,10 +1,12 @@
 import userSagas from 'sagas/user/sagas';
 import { all } from 'redux-saga/effects';
 import authSaga from "./auth/sagas";
+import formsSaga from "./forms/sagas";
 
 export default function* rootSaga() {
   yield all([
     authSaga(),
-    userSagas()
+    userSagas(),
+    formsSaga()
   ]);
 }
