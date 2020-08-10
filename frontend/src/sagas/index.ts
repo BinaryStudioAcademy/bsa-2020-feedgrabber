e10e96d8ba5a6ba87e9216fdb8ebc91dbc77a46e
Ivan Husarov <ivan.husarov@gmail.com>
2020-08-10 12:07:31 +0300

    questionnaire sagas

--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,10 +1,12 @@
 import userSagas from 'sagas/user/sagas';
 import { all } from 'redux-saga/effects';
 import authSaga from "./auth/sagas";
+import questionnaireSagas from './qustionnaires/sagas';
 
 export default function* rootSaga() {
   yield all([
     authSaga(),
-    userSagas()
+    userSagas(),
+    questionnaireSagas()
   ]);
 }
