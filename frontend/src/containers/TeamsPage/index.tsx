a977f25b3f49a20fcde5f10fe1136739a9b08a93
nick <nick>
2020-08-11 14:38:39 +0300

    remove loaders

--- frontend/src/containers/TeamsPage/index.tsx
+++ frontend/src/containers/TeamsPage/index.tsx
@@ -1,40 +1,26 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC, useEffect} from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import TeamsList from '../../components/TeamsList';
-import { ITeam } from 'models/teams/ITeam';
-import {Dimmer, Loader} from "semantic-ui-react";
 import {IAppState} from "../../models/IAppState";
 import {loadTeamsRoutine} from "../../sagas/teams/routines";
 
-const TeamList: FC<ITeamListProps> = ({ teams, loadTeams, isLoading }) => {
-  const [teamList, setTeamsList] = useState<ITeam[]>(teams);
+const TeamList: FC<ITeamListProps> = ({teams, loadTeams, isLoading}) => {
+    useEffect(() => {
+        loadTeams();
+    }, [loadTeams]);
 
-  useEffect(() => {
-    loadTeams();
-  }, [loadTeams]);
-
-  useEffect(() => {
-      setTeamsList(teams);
-  }, [teams]);
-
-  return (
-    isLoading
-      ? <Dimmer active>
-            <Loader content='Loading' />
-        </Dimmer>
-      : <TeamsList teams={teamList} />
-  );
+    return <TeamsList teams={teams}/>;
 };
 
 const mapState = (state: IAppState) => {
-  return {
-    teams: state.teams.teams,
-    isLoading: state.teams.isLoading
-  };
+    return {
+        teams: state.teams.teams,
+        isLoading: state.teams.isLoading
+    };
 };
 
 const mapDispatch = {
-  loadTeams: loadTeamsRoutine
+    loadTeams: loadTeamsRoutine
 };
 
 const connector = connect(mapState, mapDispatch);
