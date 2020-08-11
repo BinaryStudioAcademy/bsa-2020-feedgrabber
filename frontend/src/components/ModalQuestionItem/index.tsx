19fd09d99a97f4c1ed1c8fdb27d7a4bf86fd8b90
nick <nick>
2020-08-11 21:39:44 +0300

    second modal draft

--- frontend/src/components/ModalQuestionItem/index.tsx
+++ frontend/src/components/ModalQuestionItem/index.tsx
@@ -1,32 +1,33 @@
-import {Card} from 'semantic-ui-react';
+import {Card, Modal} from 'semantic-ui-react';
 import styles from './styles.module.sass';
-import React, {FC, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import {IQuestion} from "../../models/forms/Questions/IQuesion";
 import {IAppState} from "../../models/IAppState";
 import {loadQuestionsRoutine} from "../../containers/QuestionsList/routines";
 import {connect, ConnectedProps} from "react-redux";
 
-interface IPropsItem {
+interface IItemProps {
     handleClick: (id: string) => void;
     question: IQuestion;
     isSelected: boolean;
 }
 
-export const ModalQuestionItem: FC<IPropsItem> = ({question, handleClick, isSelected}) => {
-    return (<div key={question.id} className={styles.questionContainer}>
-        <Card className={styles.question}
-              link centered fluid
-              onClick={() => handleClick(question.id)}>
-            <Card.Content className={styles.content}>
-                <Card.Meta>{question.categoryTitle}</Card.Meta>
-                <Card.Description>{question.name}</Card.Description>
-                <Card.Meta className={styles.right}><span>{question.type}</span></Card.Meta>
-            </Card.Content>
-        </Card>
-    </div>);
+export const ModalQuestionItem: FC<IItemProps> = ({question, handleClick, isSelected}) => {
+    return (
+        <div key={question.id} className={styles.questionContainer}>
+            <Card className={styles.question}
+                  link centered fluid
+                  onClick={() => handleClick(question.id)}>
+                <Card.Content className={styles.content}>
+                    <Card.Meta>{question.categoryTitle}</Card.Meta>
+                    <Card.Description>{question.name}</Card.Description>
+                    <Card.Meta className={styles.right}>{question.type}</Card.Meta>
+                </Card.Content>
+            </Card>
+        </div>);
 };
 
-export const SelectQuestionsFromExisting: FC<ContainerProps> = ({questions}) => {
+export const SelectQuestionsFromExisting: FC<ContainerProps> = ({questions, loadQuestions}) => {
     const [selected, setSelected] = useState([] as IQuestion[]);
 
     const handleClick = id => {
@@ -35,16 +36,24 @@ export const SelectQuestionsFromExisting: FC<ContainerProps> = ({questions}) => 
         ]);
     };
 
+    useEffect(() => {
+        loadQuestions();
+    }, [loadQuestions]);
+
     return (
-        <>
-            {questions.map(q => <ModalQuestionItem
-                handleClick={handleClick}
-                question={q}
-                isSelected={selected.includes(q)}
-                />
-                )}
-        </>
-    );
+        <Modal>
+            <Modal.Content scrolling>
+                <Modal.Description>
+                    {questions.map(q => <ModalQuestionItem
+                            handleClick={handleClick}
+                            question={q}
+                            isSelected={selected.includes(q)}/>
+                    )}
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+            </Modal.Actions>
+        </Modal>);
 };
 
 const mapState = (state: IAppState) => ({
