00944e0ba5b277bad605d35e255d34589a616385
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-11 17:43:32 +0300

    add QuestionnaireResponse and ResponseAnswer models

---
+++ backend/core/src/main/java/com/feed_grabber/core/questionnaireResponse/model/ResponseAnswer.java
@@ -0,0 +1,39 @@
+package com.feed_grabber.core.questionnaireResponse.model;
+
+import com.feed_grabber.core.question.model.Question;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "response_answer")
+public class ResponseAnswer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column
+    private String text;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn(name = "response_id")
+    private QuestionnaireResponse response;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
