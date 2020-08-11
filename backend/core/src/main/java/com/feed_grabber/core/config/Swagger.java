f626b8d3a5e27faf756fe46aedf7632d6d945938
lenaz <lena.zelenaya@gmail.com>
2020-08-11 22:13:39 +0300

    Add final config for Docket bean

--- backend/core/src/main/java/com/feed_grabber/core/config/Swagger.java
+++ backend/core/src/main/java/com/feed_grabber/core/config/Swagger.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
