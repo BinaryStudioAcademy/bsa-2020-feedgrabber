327679c38eb52900ba37ce78d031d12b7e44ffd6
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-08-13 09:18:34 +0300

    Seed question_questionnare table

---
+++ backend/core/src/main/resources/db/seeds/V2020.08.12_23.32__seed_question_questionare_table.sql
@@ -0,0 +1,6 @@
+insert into question_questionnaire
+(questionnaire_id, question_id)
+values
+('e06193ae-dafa-11ea-87d0-0242ac130003', '31c429b6-4f13-4f8b-926d-d4ccd6c30ec0')
+('e06193ae-dafa-11ea-87d0-0242ac130003', '7d44b4b0-daf8-11ea-87d0-0242ac130003')
+('e06193ae-dafa-11ea-87d0-0242ac130003', '80f43b08-daf8-11ea-87d0-0242ac130003')
