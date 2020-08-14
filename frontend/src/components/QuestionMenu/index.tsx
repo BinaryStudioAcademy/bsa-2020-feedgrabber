ec091874669bfe017f274fc11cf03755db345332
lenaz <lena.zelenaya@gmail.com>
2020-08-14 13:48:29 +0300

    Complete with menu

--- frontend/src/components/QuestionMenu/index.tsx
+++ frontend/src/components/QuestionMenu/index.tsx
@@ -1,39 +1,47 @@
-import React, { useEffect, useState, FC } from "react";
-import { Button, Form, Segment, Modal } from "semantic-ui-react";
+import React, { FC, useState } from "react";
+import { Button, Form, Popup } from "semantic-ui-react";
 import { IAppState } from "../../models/IAppState";
-import { ConnectedProps, connect } from "react-redux";
+import { connect, ConnectedProps } from "react-redux";
 import SelectQuestionsFromExisting from "../SelectQuestionsFromExisting";
-import { deleteQuestionnaireRoutine } from "sagas/qustionnaires/routines";
-import { addNewQuestionRoutine } from "sagas/questions/routines";
+import { deleteQuestionRoutine } from "sagas/qustionnaires/routines";
+import { addSelectedQuestionsRoutine, saveQuestionRoutine } from "sagas/questions/routines";
+import { IQuestion } from "../../models/forms/Questions/IQuesion";
 
-const QuestionMenu: FC<ContainerProps> = ({ deleteQuestionnaire, addNewQuestion }) => {
-
-    const onAdd = () => {
-        addNewQuestion();
+const QuestionMenu: FC<ContainerProps> = ({ deleteQuestion, addQuestion, saveQuestion, currentQuestion }) => {
+    const handleAdd = (id: string) => {
+        return;
     };
 
-    const onDelete = () => {
-        deleteQuestionnaire();
+    const handleDelete = () => {
+        return;
     };
 
     return (
         <Form>
-            <Button.Group vertical labeled icon>
-                <Button icon="plus circle" content="New question" onClick={() => onAdd()} />
+            <Button.Group vertical>
+                <Popup content='New question' 
+                    trigger={<Button icon="plus circle" onClick={() => handleAdd("new")} />}
+                    position='right center'/>
                 <SelectQuestionsFromExisting />
-                <Button icon="rem" content="Delete" onClick={() => onDelete()} />
+                <Popup content='Copy' 
+                    trigger={<Button icon="copy" onClick={() => handleAdd("copy")}/>} 
+                    position='right center'/>
+                <Popup content='Delete' 
+                    trigger={<Button icon="remove" onClick={() => handleDelete()} />} 
+                    position='right center'/>
             </Button.Group>
         </Form>
     );
 };
 
 const mapState = (state: IAppState) => ({
-    currentQuestionnaire: state.questionnaires.current.get
+    currentQuestion: state.questionnaires.currentQuestion
 });
 
 const mapDispatch = {
-    deleteQuestionnaire: deleteQuestionnaireRoutine,
-    addNewQuestion: addNewQuestionRoutine
+    deleteQuestion: deleteQuestionRoutine,
+    addQuestion: addSelectedQuestionsRoutine,
+    saveQuestion: saveQuestionRoutine
 };
 
 const connector = connect(mapState, mapDispatch);
