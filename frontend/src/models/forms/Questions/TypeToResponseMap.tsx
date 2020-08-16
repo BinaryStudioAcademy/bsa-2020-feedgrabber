f7e8815ebfbb4b3de76ae0ed8cee0554d133663d
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-16 11:12:27 +0300

    add DateSelectionResponse to TypeToResponsemap

--- frontend/src/models/forms/Questions/TypeToResponseMap.tsx
+++ frontend/src/models/forms/Questions/TypeToResponseMap.tsx
@@ -1,10 +1,14 @@
 import { FreeTextResponse } from "components/ResponseQuestion/FreeTextResponse";
+import { DateSelectionResponse } from "components/ResponseQuestion/DateSelectionResponse";
 import { IQuestionResponse } from "models/IQuestionResponse";
 import React, { FC } from "react";
 
 export default new Map([
     ['FREE_TEXT', ({ question, answerHandler }: IQuestionResponse) => {
         return <FreeTextResponse question={question} answerHandler={answerHandler} />;
+    }],
+    ['DATE', ({ question, answerHandler }: IQuestionResponse) => {
+        return <DateSelectionResponse question={question} answerHandler={answerHandler} />;
     }]
     // ,['RADIO', ({ question, answerHandler }: IQuestionResponse) => {
     //    return <RadioResponse question={question} answerHandler={answerHandler} />;
