06ca0661f5fad76fcc4741c875d38b0813ba379f
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-16 21:42:22 +0300

    Add radio option to map

--- frontend/src/models/forms/Questions/TypeToResponseMap.tsx
+++ frontend/src/models/forms/Questions/TypeToResponseMap.tsx
@@ -1,11 +1,15 @@
-import { FreeTextResponse } from "components/ResponseQuestion/FreeTextResponse";
-import { IQuestionResponse } from "models/IQuestionResponse";
+import {FreeTextResponse} from "components/ResponseQuestion/FreeTextResponse";
+import {IQuestionResponse} from "models/IQuestionResponse";
 import React from "react";
-import { ITextQuestion } from "./IQuesion";
+import {IRadioQuestion, ITextQuestion} from "./IQuesion";
+import RadioButtonResponse from "../../../components/ResponseQuestion/RadioButtonResponse";
 
-export default new Map([
-    ['FREE_TEXT', ({ question, answerHandler }: IQuestionResponse<ITextQuestion>) => {
-        return <FreeTextResponse question={question} answerHandler={answerHandler} />;
+export default new Map<string, ({question, answerHandler}: IQuestionResponse<any>) => any>([
+    ['FREE_TEXT', ({question, answerHandler}: IQuestionResponse<ITextQuestion>) => {
+        return <FreeTextResponse question={question} answerHandler={answerHandler}/>;
+    }],
+    ['RADIO', ({question, answerHandler}: IQuestionResponse<IRadioQuestion>) => {
+        return <RadioButtonResponse question={question} answerHandler={answerHandler}/>;
     }]
     // ,['RADIO', ({ question, answerHandler }: IQuestionResponse) => {
     //    return <RadioResponse question={question} answerHandler={answerHandler} />;
