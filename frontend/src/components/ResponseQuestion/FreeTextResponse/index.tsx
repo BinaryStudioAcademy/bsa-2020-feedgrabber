5763ad0f97d5dc70faa40017a3f2d6e188bc0a00
Ivan Husarov <ivan.husarov@gmail.com>
2020-08-16 16:54:26 +0300

    flow fix + validate

--- frontend/src/components/ResponseQuestion/FreeTextResponse/index.tsx
+++ frontend/src/components/ResponseQuestion/FreeTextResponse/index.tsx
@@ -1,24 +1,25 @@
-import UIButton from "components/UI/UIButton";
 import { ITextQuestion } from "models/forms/Questions/IQuesion";
 import { IQuestionResponse } from "models/IQuestionResponse";
 import React, { FC, useState } from "react";
-import { Button, Input, InputOnChangeData } from "semantic-ui-react";
+import { Input, InputOnChangeData } from "semantic-ui-react";
 import styles from "./styles.module.sass";
 
 export const FreeTextResponse: FC<IQuestionResponse<ITextQuestion>> = ({ question, answerHandler }) => {
-    const [val, setVal] = useState("");
-    const handleChange = (e, v: InputOnChangeData) => {
-        setVal(v.value);
+    const [invalidMessage, setInvalidMessage] = useState('');
+
+    const validate = (value: string) => {
+        value.trim() ? setInvalidMessage('') : setInvalidMessage('Cannot be blank');
     };
-    return <><Input
-        value={val}
+
+    const handleChange = (e, v: InputOnChangeData) => {
+        const { value } = v;
+        validate(value);
+        answerHandler?.(question.id, !invalidMessage ? value : null);
+    };
+    return <Input
         onChange={handleChange}
         placeholder='Answer field'
+        error={!!invalidMessage}
         className={styles.input}
-    />
-        <UIButton
-            title='Submit'
-            disabled={!val}
-            onClick={() => answerHandler?.(question.id, { payload: val })} />
-    </>;
+    />;
 };
