49baf29e4c307c60eca2727ac8b3278c7801050c
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-16 14:38:07 +0300

    improve TypeToResponseMap

--- frontend/src/models/forms/Questions/TypeToResponseMap.tsx
+++ frontend/src/models/forms/Questions/TypeToResponseMap.tsx
@@ -4,27 +4,14 @@ import { IQuestionResponse } from "models/IQuestionResponse";
 import React from "react";
 import {IDateQuestion, ITextQuestion} from "./IQuesion";
 
-export default new Map([
+export default new Map<string, ({ question, answerHandler }: IQuestionResponse<any>) => any>([
     ['FREE_TEXT', ({ question, answerHandler }: IQuestionResponse<ITextQuestion>) => {
         return <FreeTextResponse question={question} answerHandler={answerHandler} />;
+    }],
+    ['DATE', ({ question, answerHandler }: IQuestionResponse<IDateQuestion>) => {
+        return <DateSelectionResponse question={question} answerHandler={answerHandler} />;
     }]
-    // ['DATE', ({ question, answerHandler }: IQuestionResponse<IDateQuestion>) => {
-    //     return <DateSelectionResponse question={question} answerHandler={answerHandler} />;
-    // }]
     // ,['RADIO', ({ question, answerHandler }: IQuestionResponse) => {
     //    return <RadioResponse question={question} answerHandler={answerHandler} />;
     // }] and other...
 ]);
-
-// my variant
-// export default new Map<string, ({ question, answerHandler }: IQuestionResponse<any>) => any>([
-//     ['FREE_TEXT', ({ question, answerHandler }: IQuestionResponse<ITextQuestion>) => {
-//         return <FreeTextResponse question={question} answerHandler={answerHandler} />;
-//     }],
-//     ['DATE', ({ question, answerHandler }: IQuestionResponse<IDateQuestion>) => {
-//         return <DateSelectionResponse question={question} answerHandler={answerHandler} />;
-//     }]
-//     // ,['RADIO', ({ question, answerHandler }: IQuestionResponse) => {
-//     //    return <RadioResponse question={question} answerHandler={answerHandler} />;
-//     // }] and other...
-// ]);
