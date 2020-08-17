639fde84a4747455a89fda80c6edfe454b9bac46
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-18 00:06:28 +0300

    now user can select only one answer in ScaleQuestionResponse; add handler for user click action

--- frontend/src/components/ResponseQuestion/ScaleQuestionResponse/index.tsx
+++ frontend/src/components/ResponseQuestion/ScaleQuestionResponse/index.tsx
@@ -5,15 +5,21 @@ import { Form, Radio } from 'semantic-ui-react';
 import styles from './styles.module.sass';
 
 export const ScaleQuestionResponse: FC<IQuestionResponse<IScaleQuestion>> =
-  ({ question= { details: { min: 1, max: 8, minDescription: 'bad', maxDescription: 'good'} },
+  ({ question= { id: '23',  details: { min: 1, max: 8, minDescription: 'bad', maxDescription: 'good'} },
      answerHandler = null }) => {
+  const handleClick = (e, value) => {
+    answerHandler?.(question.id, value);
+  };
+
   const getVariants = (min: number, max: number) => {
+    const group = question.id;
+    console.log(group);
     const variants = [];
     for (let i = min; i <= max; i++) {
       variants.push(
         <Form.Field className={styles.scale_question_response_variant} key={i}>
           <span>{i}</span>
-          <Radio value={i} />
+          <Radio value={i} name={group} onChange={handleClick} />
         </Form.Field>
       );
     }
