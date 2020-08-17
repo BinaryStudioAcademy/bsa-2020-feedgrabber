5403d719a5798f5ad664808cd749dc3578cbc267
AlexandrKarpachov <xalexandrx9@gmail.com>
2020-08-17 23:17:49 +0300

    completed

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -5,7 +5,7 @@ import Landing from "../../components/Landing";
 import PrivateRoute from "../../components/PrivateRoute";
 import MainPage from "../../components/MainPage";
 import SignForm from "../../components/AuthForm/SignForm";
-import {Profile, ProfileX} from "../../components/Profile";
+import {Profile, ProfileX, AvatarSettings} from "../../components/Profile";
 import QuestionsList from "../QuestionsList";
 import TeamsPage from "../TeamsPage";
 import QuestionnaireList from "../QuestionnaireList";
@@ -53,6 +53,7 @@ const Routing: FC<IRoutingProps> = ({ isLoading, user, getUser }) => {
           <PrivateRoute exact path="/" component={MainPage} />
           <PrivateRoute exact path="/profile" component={Profile} />
           <PrivateRoute exact path="/profile/settings" component={ProfileX} />
+          <PrivateRoute exact path="/profile/avatar" component={AvatarSettings} />
           <PrivateRoute exact path="/requests" component={() => <span>Requests</span>} />
           <PrivateRoute exact path="/help" component={() => <span>Help Center</span>} />
           <PrivateRoute exact path="/editor" component={() => <span>Form Editor</span>} />
