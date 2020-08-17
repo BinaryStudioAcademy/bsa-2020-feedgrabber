d1436e419215530e608fa00429fcb2ea6d293f24
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-18 00:14:16 +0300

    add scale question to TypeToResponseMap

--- frontend/src/models/forms/Questions/TypeToResponseMap.tsx
+++ frontend/src/models/forms/Questions/TypeToResponseMap.tsx
@@ -1,9 +1,10 @@
 import { FreeTextResponse } from "components/ResponseQuestion/FreeTextResponse";
 import { DateSelectionResponse } from "components/ResponseQuestion/DateSelectionResponse";
+import { ScaleQuestionResponse } from "components/ResponseQuestion/ScaleQuestionResponse";
 import { IQuestionResponse } from "models/IQuestionResponse";
 import RadioButtonResponse from "../../../components/ResponseQuestion/RadioButtonResponse";
 import React from "react";
-import {IRadioQuestion, IDateQuestion, ITextQuestion} from "./IQuesion";
+import { IRadioQuestion, IDateQuestion, ITextQuestion, IScaleQuestion } from "./IQuesion";
 
 export default new Map<string, ({ question, answerHandler }: IQuestionResponse<any>) => any>([
     ['FREE_TEXT', ({ question, answerHandler }: IQuestionResponse<ITextQuestion>) => {
@@ -14,6 +15,9 @@ export default new Map<string, ({ question, answerHandler }: IQuestionResponse<a
     }],
     ['RADIO', ({question, answerHandler}: IQuestionResponse<IRadioQuestion>) => {
         return <RadioButtonResponse question={question} answerHandler={answerHandler}/>;
+    }],
+    ['SCALE', ({question, answerHandler}: IQuestionResponse<IScaleQuestion>) => {
+        return <ScaleQuestionResponse question={question} answerHandler={answerHandler}/>;
     }]
     // ,['RADIO', ({ question, answerHandler }: IQuestionResponse) => {
     //    return <RadioResponse question={question} answerHandler={answerHandler} />;
