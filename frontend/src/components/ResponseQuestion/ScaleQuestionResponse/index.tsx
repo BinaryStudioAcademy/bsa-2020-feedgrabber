7bae4d6e49ab82f4ac7f5a5b43d00742269b6e5c
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-08-17 23:43:37 +0300

    add styles for ScaleQuestionResponse component

--- frontend/src/components/ResponseQuestion/ScaleQuestionResponse/index.tsx
+++ frontend/src/components/ResponseQuestion/ScaleQuestionResponse/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { IQuestionResponse } from "../../../models/IQuestionResponse";
 import { IScaleQuestion } from "../../../models/forms/Questions/IQuesion";
 import { Form, Radio } from 'semantic-ui-react';
+import styles from './styles.module.sass';
 
 export const ScaleQuestionResponse: FC<IQuestionResponse<IScaleQuestion>> =
   ({ question= { details: { min: 1, max: 8, minDescription: 'bad', maxDescription: 'good'} },
@@ -10,7 +11,7 @@ export const ScaleQuestionResponse: FC<IQuestionResponse<IScaleQuestion>> =
     const variants = [];
     for (let i = min; i <= max; i++) {
       variants.push(
-        <Form.Field>
+        <Form.Field className={styles.scale_question_response_variant} key={i}>
           <span>{i}</span>
           <Radio value={i} />
         </Form.Field>
@@ -20,10 +21,14 @@ export const ScaleQuestionResponse: FC<IQuestionResponse<IScaleQuestion>> =
   };
 
   return (
-    <Form>
-      <span>{question.details.minDescription}</span>
+    <Form className={styles.scale_question_response_form}>
+      <span className={styles.scale_question_response_description}>
+        {question.details.minDescription}
+      </span>
       {getVariants(question.details.min, question.details.max)}
-      <span>{question.details.maxDescription}</span>
+      <span className={styles.scale_question_response_description}>
+        {question.details.maxDescription}
+      </span>
     </Form>
   );
 };
