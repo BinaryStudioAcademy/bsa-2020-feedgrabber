227812aecea3ad21e3fd272ad1d86d4ebb29b321
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-18 14:11:32 +0300

    Fix

--- backend/core/src/main/java/com/feed_grabber/core/image/ImageService.java
+++ backend/core/src/main/java/com/feed_grabber/core/image/ImageService.java
@@ -2,7 +2,6 @@ package com.feed_grabber.core.image;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.feed_grabber.core.exceptions.NotFoundException;
 import com.feed_grabber.core.image.dto.ImageDto;
 import com.feed_grabber.core.image.dto.ImageUploadDto;
 import com.feed_grabber.core.image.dto.ImgurResponse;
@@ -14,28 +13,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.UUID;
 
 @Service
 public class ImageService {
 
     @Value(value = "${imgur.id}")
     private String IMGUR_ID;
+    @Value(value = "${imgur.endpoint}")
+    private static final String IMGUR_URL = "https://api.imgur.com/3/image";
     @Autowired
     ImageRepository imageRepository;
 
     public ImageDto upload(ImageUploadDto fileDto) throws IOException {
-        byte[] bytes;
-        if (fileDto.getWidth() !=null && fileDto.getHeight() !=null) {
-            bytes = cropImage(fileDto);
-        } else {
-            bytes = fileDto.getFile().getBytes();
-        }
+        byte[] bytes = fileDto.getFile().getBytes();
         var result = this.uploadFile(bytes);
         var image = new Image();
         image.setLink(result.getData().getLink());
@@ -54,25 +45,10 @@ public class ImageService {
 
         var requestEntity = new HttpEntity<>(body, headers);
 
-        var serverUrl = "https://api.imgur.com/3/image";
-
         var restTemplate = new RestTemplate();
-        var response = restTemplate.postForEntity(serverUrl, requestEntity, String.class);
+        var response = restTemplate.postForEntity(IMGUR_URL, requestEntity, String.class);
         var json = response.getBody();
         var mapper = new ObjectMapper();
         return mapper.readValue(json, ImgurResponse.class);
     }
-
-    public byte[] cropImage(ImageUploadDto fileDto) {
-        try {
-            BufferedImage image = ImageIO.read(fileDto.getFile().getInputStream()).getSubimage(fileDto.getX(), fileDto.getY(), fileDto.getWidth(), fileDto.getHeight());
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(image, "jpg", baos);
-            return baos.toByteArray();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
 }
