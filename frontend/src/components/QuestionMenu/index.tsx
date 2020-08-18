584775c93f458605b8e307f4cd84f87e3f975abb
Ivan Husarov <ivan.husarov@gmail.com>
2020-08-18 14:07:40 +0300

    vibing menu

--- frontend/src/components/QuestionMenu/index.tsx
+++ frontend/src/components/QuestionMenu/index.tsx
@@ -11,12 +11,12 @@ import {
 import styles from "./styles.module.sass";
 
 const QuestionMenu: FC<ContainerProps> = ({
-                                              deleteQuestion,
-                                              addQuestion,
-                                              currentQuestion
+    deleteQuestion,
+    addQuestion,
+    currentQuestion
 }) => {
     const handleAdd = (id: string) => {
-        if(id === "new") {
+        if (id === "new") {
             addQuestion(null);
         } else {
             addQuestion(id);
@@ -28,20 +28,26 @@ const QuestionMenu: FC<ContainerProps> = ({
     };
 
     return (
-        <Form className={styles.question_menu_container}>
-            <Button.Group vertical>
-                <Popup content='New question' 
-                    trigger={<Button icon="plus circle" onClick={() => handleAdd("new")} />}
-                    position='right center'/>
-                <SelectQuestionsFromExisting />
-                <Popup content='Copy' 
-                    trigger={<Button icon="copy" onClick={() => handleAdd(currentQuestion.id)}/>}
-                    position='right center'/>
-                <Popup content='Delete' 
-                    trigger={<Button icon="remove" onClick={() => handleDelete()} />} 
-                    position='right center'/>
-            </Button.Group>
-        </Form>
+        <div style={{
+            position: 'absolute',
+            top: currentQuestion.top,
+            transition: 'all 0.5s linear'
+        }}>
+            <Form className={styles.question_menu_container}>
+                <Button.Group vertical>
+                    <Popup content='New question'
+                        trigger={<Button icon="plus circle" onClick={() => handleAdd("new")} />}
+                        position='right center' />
+                    <SelectQuestionsFromExisting />
+                    <Popup content='Copy'
+                        trigger={<Button icon="copy" onClick={() => handleAdd(currentQuestion.id)} />}
+                        position='right center' />
+                    <Popup content='Delete'
+                        trigger={<Button icon="remove" onClick={() => handleDelete()} />}
+                        position='right center' />
+                </Button.Group>
+            </Form>
+        </div>
     );
 };
 
