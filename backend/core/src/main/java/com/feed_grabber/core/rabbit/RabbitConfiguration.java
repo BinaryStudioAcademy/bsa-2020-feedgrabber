31ca155782b44973697ac5088714a861244fa679
Ivan Husarov <ivan.husarov@gmail.com>
2020-08-18 21:01:46 +0300

    fix connect to questionnaire and add new default

--- backend/core/src/main/java/com/feed_grabber/core/rabbit/RabbitConfiguration.java
+++ backend/core/src/main/java/com/feed_grabber/core/rabbit/RabbitConfiguration.java
@@ -1,76 +1,76 @@
-package com.feed_grabber.core.rabbit;
-
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
-import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
-
-@Configuration
-@EnableRabbit
-public class RabbitConfiguration implements RabbitListenerConfigurer {
-    @Value("${rabbitmq.exchange}")
-    private String exchange;
-
-    @Value("${rabbitmq.routing-key-response}")
-    private String routingKey;
-
-    @Value("${rabbitmq.queue.response}")
-    private String queue;
-
-    @Bean
-    public Queue queue() {
-        return new Queue(queue, true);
-    }
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(exchange);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
-        return rabbitTemplate;
-    }
-
-    @Override
-    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
-        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
-    }
-
-    @Bean
-    public MessageHandlerMethodFactory messageHandlerMethodFactory() {
-        DefaultMessageHandlerMethodFactory messageHandlerMethodFactory
-                = new DefaultMessageHandlerMethodFactory();
-        messageHandlerMethodFactory.setMessageConverter(consumerJackson2MessageConverter());
-        return messageHandlerMethodFactory;
-    }
-
-    @Bean
-    public MappingJackson2MessageConverter consumerJackson2MessageConverter() {
-        return new MappingJackson2MessageConverter();
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-}
+//package com.feed_grabber.core.rabbit;
+//
+//import org.springframework.amqp.core.Binding;
+//import org.springframework.amqp.core.BindingBuilder;
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.amqp.core.TopicExchange;
+//import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+//import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+//import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+//import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
+//
+//@Configuration
+//@EnableRabbit
+//public class RabbitConfiguration implements RabbitListenerConfigurer {
+//    @Value("${rabbitmq.exchange}")
+//    private String exchange;
+//
+//    @Value("${rabbitmq.routing-key-response}")
+//    private String routingKey;
+//
+//    @Value("${rabbitmq.queue.response}")
+//    private String queue;
+//
+//    @Bean
+//    public Queue queue() {
+//        return new Queue(queue, true);
+//    }
+//
+//    @Bean
+//    public TopicExchange exchange() {
+//        return new TopicExchange(exchange);
+//    }
+//
+//    @Bean
+//    public Binding binding(Queue queue, TopicExchange exchange) {
+//        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+//    }
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+//        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+//        return rabbitTemplate;
+//    }
+//
+//    @Override
+//    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+//        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
+//    }
+//
+//    @Bean
+//    public MessageHandlerMethodFactory messageHandlerMethodFactory() {
+//        DefaultMessageHandlerMethodFactory messageHandlerMethodFactory
+//                = new DefaultMessageHandlerMethodFactory();
+//        messageHandlerMethodFactory.setMessageConverter(consumerJackson2MessageConverter());
+//        return messageHandlerMethodFactory;
+//    }
+//
+//    @Bean
+//    public MappingJackson2MessageConverter consumerJackson2MessageConverter() {
+//        return new MappingJackson2MessageConverter();
+//    }
+//
+//    @Bean
+//    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+//        return new Jackson2JsonMessageConverter();
+//    }
+//}
