81d0cebbba06e93869af47f17dd08727164033c7
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-08-19 07:33:07 +0300

    QuestionDetails design updated

--- frontend/src/components/QuestionDetails/index.tsx
+++ frontend/src/components/QuestionDetails/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Form, Segment} from "semantic-ui-react";
+import {Divider, Dropdown, Form, Icon, Radio, Segment} from "semantic-ui-react";
 import {Formik} from "formik";
 import styles from "./styles.module.sass";
 import {IQuestion, QuestionType} from "../../models/forms/Questions/IQuesion";
@@ -10,9 +10,10 @@ import ScaleQuestion from "../ComponentsQuestions/ScaleQuestion";
 import DateSelectionQuestionUI from "../ComponentsQuestions/DateSelectionQuestionUI";
 import FileUploadQuestion from "../ComponentsQuestions/FileUploadQuestion";
 import {mainSchema} from "./schemas";
-import {questionTypeOptions} from "./questionTypeOptions";
+// import {questionTypeOptions} from "./questionTypeOptions";
 import RadioButtonQuestionUI from "../ComponentsQuestions/RadioButtonQuestionUI";
 import FreeTextQuestionUI from "../ComponentsQuestions/FreeTextQuestionUI";
+import QuestionDetailsOptions from "./QuestionDetailsOptions";
 
 interface IQuestionProps {
     currentQuestion: IQuestion;
@@ -131,26 +132,28 @@ const QuestionD: React.FC<IQuestionProps> = ({
             {formik => (
                 <div className={styles.question_container}>
                     <Form className={styles.question_form}>
-
                         <Segment className={styles.question_header}>
-                            <Form.Input
-                                className="question_name_input"
-                                fluid
-                                placeholder="Type your question"
-                                type="text"
-                                value={formik.values.name}
-                                name="name"
-                                error={
-                                    formik.touched.name && formik.errors.name
-                                        ? formik.errors.name
-                                        : undefined
-                                }
-                                onChange={(e, {value}) => {
-                                    setNameIsValid(value.length > 0);
-                                    handleQuestionUpdate({...question, name: value as string});
-                                }}
-                                onBlur={formik.handleBlur}
-                            />
+                            <div className={styles.question_input_type}>
+                                <Form.Input
+                                    className={styles.question_input}
+                                    fluid
+                                    placeholder="Type your question"
+                                    type="text"
+                                    value={formik.values.name}
+                                    name="name"
+                                    error={
+                                        formik.touched.name && formik.errors.name
+                                            ? formik.errors.name
+                                            : undefined
+                                    }
+                                    onChange={(e, {value}) => {
+                                        setNameIsValid(value.length > 0);
+                                        handleQuestionUpdate({...question, name: value as string});
+                                    }}
+                                    onBlur={formik.handleBlur}
+                                />
+                                <QuestionDetailsOptions question={question} setQuestionType={setQuestionType} />
+                            </div>
                             <Form.Dropdown
                                 placeholder='Choose category or type custom'
                                 closeOnBlur
@@ -179,19 +182,34 @@ const QuestionD: React.FC<IQuestionProps> = ({
                                     [...addedCategories, ...categories])}
                                 onBlur={formik.handleBlur}
                             />{' '}
-                            <Form.Dropdown
-                                selection
-                                options={questionTypeOptions}
-                                value={question.type}
-                                placeholder={"Choose type"}
-                                onChange={(event, data) => setQuestionType(data)}
-                            />
+                            <Divider />
+                            <div className={styles.question_form_answers}>
+                                {renderForm()}
+                            </div>
+                            <Divider />
+                            <div className={styles.actions}>
+                                <span className={styles.icon}>
+                                    <Icon name="plus square outline" size="large" />
+                                </span>
+                                <span className={styles.icon}>
+                                    <Icon name="trash alternate outline" size="large" />
+                                </span>
+                                <Radio toggle />
+                                <Dropdown
+                                    className={styles.icon}
+                                    text=" "
+                                    icon="ellipsis vertical"
+                                    options={[
+                                        {
+                                            text: "option1"
+                                        },
+                                        {
+                                            text: "option2"
+                                        }
+                                    ]}
+                                />
+                            </div>
                         </Segment>
-
-                        <Segment className={styles.question_form_answers}>
-                            {renderForm()}
-                        </Segment>
-
                     </Form>
                 </div>
             )}
