33e2f0d22c46e5c61083bc7e331cdda220fd3bd0
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-20 09:17:55 +0300

    Fix public read access

--- backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/fileStorage/AmazonS3ClientService.kt
+++ backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/fileStorage/AmazonS3ClientService.kt
@@ -6,6 +6,7 @@ import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.time.LocalDateTime
 import java.util.*
@@ -25,7 +26,7 @@ class AmazonS3ClientService(private val s3Client: S3Client) {
 
     fun putObject(resource: Resource, prefix: String): String {
         val key = prefix + LocalDateTime.now() + "-" + resource.filename?.replace(" ","_") ?: UUID.randomUUID().toString()
-        val request = PutObjectRequest.builder().bucket(BUCKET_NAME).key(key).build()
+        val request = PutObjectRequest.builder().bucket(BUCKET_NAME).key(key).acl(ObjectCannedACL.PUBLIC_READ).build()
         val requestBody = RequestBody.fromInputStream(resource.inputStream, resource.contentLength())
         s3Client.putObject(request, requestBody)
         return "$ENDPOINT$BUCKET_NAME/$key"
