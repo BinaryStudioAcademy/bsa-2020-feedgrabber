0be102277bc0bfabe42feacfd4a827a79344f502
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-21 12:30:35 +0300

    Update senders and receivers

--- backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/rabbit/Receiver.kt
+++ backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/rabbit/Receiver.kt
@@ -2,13 +2,16 @@ package com.feed_grabber.event_processor.rabbit
 
 import com.feed_grabber.event_processor.rabbit.entityExample.MailEntity
 import com.feed_grabber.event_processor.email.EmailSender
+import com.feed_grabber.event_processor.report.excel.ExcelReportGenerator
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
 
 @Component
 class Receiver(
-	@Autowired val emailSender: EmailSender
+	@Autowired val emailSender: EmailSender,
+	@Autowired val excelReportGenerator: ExcelReportGenerator
 ) {
     @RabbitListener(queues = ["\${rabbitmq.queue}"])
     fun receive(mailEntity: MailEntity?) {
@@ -16,4 +19,9 @@ class Receiver(
 		println(mailEntity?.getMessage())
 		emailSender.sendMail(mailEntity)
     }
+
+	@RabbitListener(queues = ["\${rabbitmq.routing-key-report-excel}"])
+	fun receiveExcelGenerationRequest(requestId: UUID) {
+	excelReportGenerator.generate(requestId);
+	}
 }
