fcc67b94962eae5f7008824fa10a602aca16ddc6
Ivan Husarov <ivan.husarov.mp.2018@lpnu.ua>
2020-08-24 18:39:00 +0300

    Not fully styles, etc

--- frontend/src/containers/RequestsPage/RequestItem/index.tsx
+++ frontend/src/containers/RequestsPage/RequestItem/index.tsx
@@ -1,16 +1,19 @@
 import UICardBlock from "components/UI/UICardBlock";
-import { IRequestShort } from "models/report/IReport";
-import React, { FC } from "react";
-import { history } from "../../../helpers/history.helper";
-import { toastr } from 'react-redux-toastr';
+import {IRequestShort} from "models/report/IReport";
+import React, {FC} from "react";
+import {history} from "../../../helpers/history.helper";
+import {toastr} from 'react-redux-toastr';
+import styles from './styles.module.sass';
 
-export const RequestItem: FC<{ request: IRequestShort }> = ({ request }) => {
-    const { requestId, isClosed, requestMaker, creationDate, generateReport } = request;
-    function handleClick(id: string, isClosed: boolean) {
-        !isClosed && history.push(`/report/${id}`);
-        isClosed && toastr.warning("This request is Closed");
+export const RequestItem: FC<{ request: IRequestShort }> = ({request}) => {
+    const {requestId, closeDate, requestMaker, creationDate, generateReport} = request;
+
+    function handleClick(id: string, closeDate: string) {
+        !closeDate && history.push(`/report/${id}`);
+        closeDate && toastr.warning("This request is Closed");
     }
-    return <UICardBlock key={requestId} onClick={() => handleClick(requestId, isClosed)}>
+
+    return <UICardBlock key={requestId} onClick={() => handleClick(requestId, closeDate)}>
         <h3>Made by {requestMaker.username}</h3>
         <h4>Created at {creationDate.substr(0, 19)}</h4>
         <h5>{generateReport && "Report will be generated automatic"}</h5>
--- frontend/src/containers/RequestsPage/index.tsx
+++ frontend/src/containers/RequestsPage/index.tsx
@@ -1,16 +1,17 @@
-import React, { FC, useEffect, useState } from "react";
-import { IAppState } from "../../models/IAppState";
-import { connect, ConnectedProps } from "react-redux";
-import { loadQuestionnaireRequestsRoutine } from "../../sagas/report/routines";
+import React, {FC, useEffect, useState} from "react";
+import {IAppState} from "../../models/IAppState";
+import {connect, ConnectedProps} from "react-redux";
+import {loadQuestionnaireRequestsRoutine} from "../../sagas/report/routines";
 import UICard from "../../components/UI/UICard";
 import UIContent from "../../components/UI/UIContent";
 import UIPageTitle from "../../components/UI/UIPageTitle";
-import { RequestItem } from "./RequestItem";
-import { Tab } from "semantic-ui-react";
-import { IRequestShort } from "models/report/IReport";
+import {RequestItem} from "./RequestItem";
+import {Tab} from "semantic-ui-react";
+import {IRequestShort} from "models/report/IReport";
+import styles from './styles.module.sass';
 
 const RequestsPage: FC<RequestPageProps & { match }> = (
-    { loadRequests, match, requests }) => {
+    {loadRequests, match, requests}) => {
 
     const [open, setOpen] = useState([] as IRequestShort[]);
     const [closed, setClosed] = useState([] as IRequestShort[]);
@@ -20,35 +21,31 @@ const RequestsPage: FC<RequestPageProps & { match }> = (
     }, [loadRequests, match.params.id]);
 
     useEffect(() => {
-        setOpen(requests.filter(r => !r.isClosed));
-        setClosed(requests.filter(r => r.isClosed));
+        setOpen(requests.filter(r => !r.closeDate));
+        setClosed(requests.filter(r => r.closeDate));
     }, [requests]);
 
     const panes = [
-        {
+        open.length && {
             menuItem: 'Pending requests',
             render: () => <Tab.Pane attached={false}>{open.map(r => (
-                <RequestItem request={r} />
+                <RequestItem request={r}/>
             ))}</Tab.Pane>
         },
-        {
+        closed.length && {
             menuItem: 'Expired requests',
             render: () => <Tab.Pane attached={false}>{closed.map(r => (
-                <RequestItem request={r} />
+                <RequestItem request={r}/>
             ))}</Tab.Pane>
         }
     ];
 
     return (
         <>
-            <UIPageTitle title="Requests" />
-            <UIContent>
-                <UICard>
-                    {
-                        <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
-                    }
-                </UICard>
-            </UIContent>
+            <UIPageTitle title="Requests"/>
+                <UIContent>
+                    <Tab menu={{secondary: true, pointing: true}} panes={panes}/>
+                </UIContent>
         </>
     );
 };
