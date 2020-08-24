f404dae3a2d88f2be3419e7090ab86b8ad11e424
Ivan Husarov <ivan.husarov.mp.2018@lpnu.ua>
2020-08-24 13:15:00 +0300

    report tabs

--- frontend/src/containers/RequestsPage/index.tsx
+++ frontend/src/containers/RequestsPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { IAppState } from "../../models/IAppState";
 import { connect, ConnectedProps } from "react-redux";
 import { loadQuestionnaireRequestsRoutine } from "../../sagas/questionnaireReport/routines";
@@ -9,13 +9,47 @@ import UICardBlock from "../../components/UI/UICardBlock";
 import { history } from "../../helpers/history.helper";
 import { toastr } from 'react-redux-toastr';
 import { RequestItem } from "./RequestItem";
+import { Tab } from "semantic-ui-react";
+import { IRequestShort } from "models/report/IReport";
 
 const RequestsPage: FC<RequestPageProps & { match }> = (
     { loadRequests, match, requests }) => {
 
+    const [divided, setDivided] = useState(
+        {
+            closed: [] as IRequestShort[],
+            open: [] as IRequestShort[]
+        });
+
     useEffect(() => {
         loadRequests(match.params.id);
     }, [loadRequests, match.params.id]);
+
+    useEffect(() => {
+        setDivided({ closed: [], open: [] });
+        requests.map(r => {
+            if (!r.isClosed) {
+                setDivided({ open: [...divided.open, r], closed: divided.closed });
+            } else {
+                setDivided({ open: divided.open, closed: [...divided.closed, r] });
+            }
+        });
+    }, [requests]);
+
+    const panes = [
+        {
+            menuItem: 'Pending requests',
+            render: () => <Tab.Pane attached={false}>{divided.open.map(r => (
+                <RequestItem request={r} />
+            ))}</Tab.Pane>
+        },
+        {
+            menuItem: 'Expired requests',
+            render: () => <Tab.Pane attached={false}>{divided.closed.map(r => (
+                <RequestItem request={r} />
+            ))}</Tab.Pane>
+        }
+    ];
 
     return (
         <>
@@ -23,9 +57,7 @@ const RequestsPage: FC<RequestPageProps & { match }> = (
             <UIContent>
                 <UICard>
                     {
-                        requests.map(r => (
-                            <RequestItem request={r} />
-                        ))
+                        <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
                     }
                 </UICard>
             </UIContent>
