7715c9c2ddbfaf5e99718b81c084bdbbc965830e
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-08-25 13:42:10 +0300

    Optimized code

--- frontend/src/helpers/subdomain.helper.ts
+++ frontend/src/helpers/subdomain.helper.ts
@@ -2,53 +2,46 @@ import {ICompanyDomain} from "../models/companies/ICompanyDomain";
 
 // `http://${domainPrefix}${companyDomain}.${domainSuffix}${port}`
 export const getDomainSuffix = () => {
-    const hostname = window.location.hostname;
-    const parts = hostname.split('.');
-    // localhost
-    if (parts[parts.length - 1].startsWith('localhost')) {
-        return 'feedgrabber.com.' + parts[parts.length - 1];
-    }
-    // e.g www.feedgrabber.com
-    else {
-        return parts.slice(parts.length - 2, parts.length).join('.');
-    }
+  const hostname = window.location.hostname;
+  const parts = hostname.split('.');
+  // localhost
+  if (parts[parts.length - 1].startsWith('localhost')) {
+    return 'feedgrabber.com.' + parts[parts.length - 1];
+  }
+  // e.g www.feedgrabber.com
+  else {
+    return parts.slice(parts.length - 2, parts.length).join('.');
+  }
 };
 
-export const getDomainPrefix = () =>  {
-    const hostname = window.location.hostname;
-    const parts = hostname.split('.');
-    // localhost
-    if (parts[parts.length - 1].startsWith('localhost')) {
-        return '';
-    }
-    // e.g www.feedgrabber.com
-    else {
-        return 'www.';
-    }
+export const getDomainPrefix = () => {
+  const hostname = window.location.hostname;
+  const parts = hostname.split('.');
+  // localhost
+  if (parts[parts.length - 1].startsWith('localhost')) {
+    return '';
+  }
+  // e.g www.feedgrabber.com
+  else {
+    return 'www.';
+  }
 };
 
 export const redirectToCompany = (company: ICompanyDomain) => {
-    const companyName = company.subdomainName;
-
-    const protocol = window.location.protocol;
-    const port = window.location.port;
-    const pathname = window.location.pathname;
-    const hostname = window.location.hostname;
-
-    const newHostname = getDomainPrefix() + companyName + '.' + getDomainSuffix();
-    if(hostname !== newHostname) {
-        window.location.replace(protocol + '//' + newHostname + ':' + port + pathname);
-    }
+  const companyName = company.subdomainName;
+  const newHostname = getDomainPrefix() + companyName + '.' + getDomainSuffix();
+  redirectToNewHostName(newHostname);
 };
 
 export const redirectToMain = () => {
-    const protocol = window.location.protocol;
-    const port = window.location.port;
-    const pathname = window.location.pathname;
-    const hostname = window.location.hostname;
+  const newHostname = getDomainPrefix() + getDomainSuffix();
+  redirectToNewHostName(newHostname);
+};
 
-    const newHostname = getDomainPrefix() + getDomainSuffix();
-    if(hostname !== newHostname) {
-        window.location.replace(protocol + '//' + newHostname + ':' + port + pathname);
-    }
+const redirectToNewHostName = (newHostName: string) => {
+  const {protocol, port, pathname, hostname} = window.location;
+
+  if (hostname !== newHostName) {
+    window.location.replace(protocol + '//' + newHostName + ':' + port + pathname);
+  }
 };
