52a9b2a9ad50522bd9a251e2f0d0bd19f0880bde
Olexander <olexandermarchuk@gmail.com>
2020-08-26 16:58:07 +0300

    uncomment

--- frontend/src/models/forms/Questions/DefaultQuestion.ts
+++ frontend/src/models/forms/Questions/DefaultQuestion.ts
@@ -1,22 +1,22 @@
 import {defaultQuestionValues} from "../../../components/QuestionDetails/defaultValues";
 import {QuestionType} from "./IQuesion";
 
-const question = {
-  id: "",
-  name: "",
-  categoryTitle: "",
-  type: undefined,
-  details: undefined,
-  isRequired: false
-};
-//
 // const question = {
 //   id: "",
-//   name: defaultQuestionValues.name,
-//   categoryTitle: defaultQuestionValues.categoryTitle,
-//   type: QuestionType.radio,
-//   details: {answerOptions: ["Option 1"], includeOther: false},
+//   name: "",
+//   categoryTitle: "",
+//   type: undefined,
+//   details: undefined,
 //   isRequired: false
 // };
+//
+const question = {
+  id: "",
+  name: defaultQuestionValues.name,
+  categoryTitle: defaultQuestionValues.categoryTitle,
+  type: QuestionType.radio,
+  details: {answerOptions: ["Option 1"], includeOther: false},
+  isRequired: false
+};
 
 export default question;
