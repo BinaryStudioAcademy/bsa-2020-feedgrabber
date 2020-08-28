34ce9cdbe42f2fbcf5b73e4ae1c1f05638ee25a4
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2020-08-28 18:41:38 +0300

    Updated deployment scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,11 @@
 
 docker login -u $docker_user -p $docker_pass
 
+cd backend/core && gradle assemble || exit 1
+cd ../event_processor && gradle assemble || exit 1
+cd ../../frontend  && npm install --no-optional && npm run build || exit 1
+cd ../
+
 docker build \
    -f ".docker/api.dockerfile" \
    -t "fg-core" \
