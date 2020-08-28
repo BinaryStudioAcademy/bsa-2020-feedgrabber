0ab2e86eb457b20abacf2e646229c61fdc4666b4
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2020-08-28 19:04:44 +0300

    Changed relative path to scripts

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #if PR on dev - run tests
 if [[ $TRAVIS_EVENT_TYPE == "pull_request" && ($TRAVIS_PULL_REQUEST_BRANCH == "dev") ]]  
-    then bash test.sh  
+    then bash scripts/test.sh  
 fi
 
 #if push on dev - deploy, else - run tests
 if [[ $TRAVIS_EVENT_TYPE == "push" ]] 
     then 
     if [[ $TRAVIS_BRANCH == "dev" ]]
-        then bash build.sh && expect deploy.sh
+        then bash scripts/build.sh && expect scripts/deploy.sh
     else
-        bash test.sh
+        bash scripts/test.sh
     fi 
 fi
