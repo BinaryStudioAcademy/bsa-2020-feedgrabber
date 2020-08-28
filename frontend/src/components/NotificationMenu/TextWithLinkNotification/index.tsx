139e2fc50525b602bdfe348aadc1bafb95e17c66
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-08-28 06:57:30 +0300

    Style text with link notification component

--- frontend/src/components/NotificationMenu/TextWithLinkNotification/index.tsx
+++ frontend/src/components/NotificationMenu/TextWithLinkNotification/index.tsx
@@ -1,33 +1,24 @@
 import React from "react";
 import {INotificationProps} from "../index";
 import {useHistory} from "react-router-dom";
-import styles from "../PlainTextNotification/styles.module.sass";
+import styles from "./styles.module.sass";
 import moment from "moment";
+import {Button, Icon} from "semantic-ui-react";
 
 const TextWithLinkNotification: React.FC<INotificationProps> = ({notification, deleteNotification, setShown}) => {
     const history = useHistory();
     return (
         <div key={notification.id}
              className={styles.notification}>
-            <div className={styles.text}
-                 onClick={() => {
-                     history.push(`/response/${notification.questionnaireId}`);
-                     deleteNotification(notification.id);
-                     setShown(false);
-                 }}>
-                <div>{notification.text?.substr(0, 54)}</div>
-                <div className={styles.date}>{moment(notification.date).fromNow()}</div>
-                <a href={notification.link}>Download</a>
+            <span className={styles.text}>{notification.text?.substr(0, 54)}</span>
+            <div className={styles.date}>{moment(notification.date).fromNow()}</div>
+            <div className={styles.download}>
+                <a href={notification.link}><Icon name={"download"}></Icon></a>
             </div>
-            <div className={styles.button}
-                 title='Delete'
-                 onClick={() => {
-                     deleteNotification(notification.id);
-                 }}
-            >
-                <div>
-                    x
-                </div>
+            <div className={styles.close}>
+                <Icon color={"grey"} name={"close"} onClick={() => {
+                    deleteNotification(notification.id);
+                }}></Icon>
             </div>
         </div>
     );
