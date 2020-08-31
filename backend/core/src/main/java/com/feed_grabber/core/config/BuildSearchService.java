ff7cd31a26463a9ee5442b1cd7543fb87f672db8
Olexander <olexandermarchuk@gmail.com>
2020-09-01 01:06:37 +0300

    delete comments

--- backend/core/src/main/java/com/feed_grabber/core/config/BuildSearchService.java
+++ backend/core/src/main/java/com/feed_grabber/core/config/BuildSearchService.java
@@ -4,9 +4,13 @@ import org.hibernate.search.jpa.Search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+@Component
+@Transactional
 public class BuildSearchService implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
     EntityManager entityManager;
