9d543644f4b25f8fb134d8c77449a06d840c54fa
AlexandrKarpachov <xalexandrx9@gmail.com>
2020-09-01 13:59:30 +0300

    refactor

--- backend/core/src/main/java/com/feed_grabber/core/question/QuestionRepository.java
+++ backend/core/src/main/java/com/feed_grabber/core/question/QuestionRepository.java
@@ -17,26 +17,6 @@ public interface QuestionRepository extends JpaRepository<Question, UUID> {
             "INNER JOIN q.questionnaires que " +
             "WHERE que.id = :questionnaireId")
     List<Question> findAllByQuestionnaireId(@Param("questionnaireId") UUID questionnaireId);
-//
-//    @Query("SELECT case when (COUNT(q) > 0) then true else false end " +
-//            "FROM Question q INNER JOIN q.questionnaires que " +
-//            "WHERE q.text = :text AND que.questionnaire.id = :questionnaireId AND q.category.id = :categoryId")
-//    boolean existsByTextAndQuestionnaireIdAndCategoryId
-//            (@Param("text") String text, @Param("questionnaireId") UUID questionnaireId,
-//             @Param("categoryId") UUID categoryId);
-
-//    @Query("SELECT case when (COUNT(q) > 0) then true else false end " +
-//            "FROM Question q INNER JOIN q.questionnaires que " +
-//            "WHERE q.text = :text AND que.questionnaire.id = :questionnaireId AND " +
-//            "q.category.id = :categoryId AND q.id <> :id")
-//    boolean existsByTextAndQuestionnaireIdAndCategoryIdAndIdIsNot
-//            (@Param("text") String text, @Param("questionnaireId") UUID questionnaireId,
-//             @Param("categoryId") UUID categoryId, @Param("id") UUID id);
-
-//    @Transactional
-//    @Modifying
-//    @Query("delete from Question where id in (select q.id from Question q inner join q.questionnaires que on (q.id = ?2 and que.questionnaire.id = ?1))")
-//    void deleteByQuestionnaireId(UUID qId, UUID id);
 
     @Query("from Question q " +
             "inner join SectionQuestion sq on q.id = sq.question.id " +
