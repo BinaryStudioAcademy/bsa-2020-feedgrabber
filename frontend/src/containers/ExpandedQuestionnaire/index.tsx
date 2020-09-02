fa649cdb7ef782004888997e5fdc570c38a24153
nick <nick>
2020-09-02 10:35:54 +0300

    form creation style fixes

--- frontend/src/containers/ExpandedQuestionnaire/index.tsx
+++ frontend/src/containers/ExpandedQuestionnaire/index.tsx
@@ -22,6 +22,8 @@ import {Header} from "semantic-ui-react";
 import LoaderWrapper from "../../components/LoaderWrapper";
 import {IQuestion} from "../../models/forms/Questions/IQuesion";
 import {toastr} from "react-redux-toastr";
+import UIPageTitle from "../../components/UI/UIPageTitle";
+import {toggleMenuRoutine} from "../../sagas/app/routines";
 
 const ExpandedQuestionnaire: React.FC<ExpandedQuestionnaireProps & { match }> = (
     {
@@ -37,7 +39,8 @@ const ExpandedQuestionnaire: React.FC<ExpandedQuestionnaireProps & { match }> = 
         createSection,
         currentSection,
         indexQuestions,
-        loadQuestion
+        loadQuestion,
+        toggleMenu
     }
 ) => {
     const [question, setQuestion] = useState<IQuestion>();
@@ -57,7 +60,8 @@ const ExpandedQuestionnaire: React.FC<ExpandedQuestionnaireProps & { match }> = 
 
     useEffect(() => {
         setQuestion(currentQuestion);
-    }, [currentQuestion]);
+        toggleMenu(false);
+    }, [currentQuestion, toggleMenu]);
 
     const addNewQuestion = () => {
         const section = currentSection ? currentSection : sections[0];
@@ -89,11 +93,7 @@ const ExpandedQuestionnaire: React.FC<ExpandedQuestionnaireProps & { match }> = 
 
     return (
         <>
-            <Header as='h1' dividing style={{padding: "1.2em", textAlign: "center"}}>
-                <Header.Content>
-                    {questionnaire.title}
-                </Header.Content>
-            </Header>
+            <UIPageTitle title={questionnaire.title} />
             {questionnaire && (
                 <div className={styles.formDetails}>
                     <LoaderWrapper loading={isLoading}>
@@ -130,6 +130,7 @@ const mapStateToProps = (rootState: IAppState) => ({
 const mapDispatchToProps = {
     loadQuestionnaire: loadSectionsByQuestionnaireRoutine,
     saveQuestion: saveQuestionRoutine,
+    toggleMenu: toggleMenuRoutine,
     deleteQuestion: deleteQuestionFromSectionRoutine,
     createSection: createSectionRoutine,
     indexQuestions: indexQuestionsRoutine,
