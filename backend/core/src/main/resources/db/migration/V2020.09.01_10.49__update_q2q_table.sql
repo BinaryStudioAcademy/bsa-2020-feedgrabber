93a39c4a7946f236fcee5ffa8045e34b268068d7
Zelenaya Elena <32305667+lenazelenaya@users.noreply.github.com>
2020-09-02 09:16:54 +0300

    Delete migration that is in dev already

--- backend/core/src/main/resources/db/migration/V2020.09.01_10.49__update_q2q_table.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists question_questionnaire;
-create table question_questionnaire
-(
-    questionnaire_id uuid not null,
-    question_id uuid not null,
-    primary key(question_id, questionnaire_id),
-    constraint fk_questionnaire2question foreign key(questionnaire_id) references questionnaires(id),
-    constraint fk_question2questionnaire foreign key(question_id) references questions(id) ON DELETE CASCADE
-);
