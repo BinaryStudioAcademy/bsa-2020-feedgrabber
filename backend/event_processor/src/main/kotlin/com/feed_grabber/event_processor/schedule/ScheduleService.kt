fce8ffe194c4eca49cf5774c279d4ed99cc6eed1
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-09-04 00:05:48 +0300

    improve schedule

--- backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/schedule/ScheduleService.kt
+++ backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/schedule/ScheduleService.kt
@@ -2,6 +2,7 @@ package com.feed_grabber.event_processor.schedule
 
 import com.feed_grabber.event_processor.schedule.jobs.buildCloseReportJob
 import com.feed_grabber.event_processor.schedule.triggers.getTrigger
+import com.feed_grabber.event_processor.rabbit.Sender
 import org.quartz.JobDetail
 import org.quartz.Scheduler
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,12 +11,13 @@ import java.util.*
 
 @Service
 class ScheduleService(
-        @Autowired val scheduler: Scheduler
+        @Autowired val scheduler: Scheduler,
+        @Autowired val sender: Sender
 ) {
     fun scheduleCloseRequestJob(requestId: UUID, closeDate: Date) {
         println("Add new job")
-        val newJob: JobDetail = buildCloseReportJob(requestId, closeDate)
-        val date: Date = scheduler.scheduleJob(newJob, getTrigger(newJob, closeDate))
+        val newJob: JobDetail = buildCloseReportJob(requestId, closeDate, sender)
+        val date: Date = scheduler.scheduleJob(newJob, getTrigger(newJob, closeDate, requestId))
         println("New job scheduled: $date")
     }
 }
--- backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/schedule/triggers/CloseReportTrigger.kt
+++ backend/event_processor/src/main/kotlin/com/feed_grabber/event_processor/schedule/triggers/CloseReportTrigger.kt
@@ -5,9 +5,9 @@ import org.quartz.Trigger
 import org.quartz.TriggerBuilder.newTrigger
 import java.util.*
 
-fun getTrigger(job: JobDetail, date: Date): Trigger {
+fun getTrigger(job: JobDetail, date: Date, id: UUID): Trigger {
     return newTrigger()
-            .withIdentity("job1", "closeRequest")
+            .withIdentity(id.toString(), "closeRequest")
 //            .withSchedule(simpleSchedule().withIntervalInSeconds(3).repeatForever())
             .startAt(date)
             .forJob(job).build()
