74e91c5283a7c8a254b06360f934ecfef9493998
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-09-03 12:35:57 +0300

    Teams page No items label, localized No items label

--- frontend/src/containers/TeamsPage/index.tsx
+++ frontend/src/containers/TeamsPage/index.tsx
@@ -2,10 +2,10 @@ import React, {FC, useEffect} from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import {IAppState} from "../../models/IAppState";
 import {
-    clearCurrentTeamRoutine,
-    deleteTeamRoutine,
-    loadCompanyUsersRoutine,
-    loadTeamsRoutine
+  clearCurrentTeamRoutine,
+  deleteTeamRoutine,
+  loadCompanyUsersRoutine,
+  loadTeamsRoutine
 } from "../../sagas/teams/routines";
 import UIPageTitle from "../../components/UI/UIPageTitle";
 import UIContent from "../../components/UI/UIContent";
@@ -18,98 +18,101 @@ import LoaderWrapper from "../../components/LoaderWrapper";
 import {history} from "../../helpers/history.helper";
 import {Permissions} from "../../components/AccessManager/rbac-rules";
 import AccessManager from "../../components/AccessManager";
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
+import styles from './styles.module.sass';
 
 const TeamsPage: FC<ITeamsPageProps> = (
-    {
-        teams,
-        loadTeams,
-        loadUsers,
-        deleteTeam,
-        clearCurrentTeam,
-        isLoading,
-        result
-    }
+  {
+    teams,
+    loadTeams,
+    loadUsers,
+    deleteTeam,
+    clearCurrentTeam,
+    isLoading,
+    result
+  }
 ) => {
-    useEffect(() => {
-        if (!teams && !isLoading) {
-            loadTeams();
-        }
-    }, [teams, isLoading, loadTeams, loadUsers]);
+  useEffect(() => {
+    if (!teams && !isLoading) {
+      loadTeams();
+    }
+  }, [teams, isLoading, loadTeams, loadUsers]);
 
-    const handleRedirect = (id: string): void => {
-        clearCurrentTeam();
-        history.push(`/teams/${id}`);
-    };
+  const handleRedirect = (id: string): void => {
+    clearCurrentTeam();
+    history.push(`/teams/${id}`);
+  };
 
-    const [t] = useTranslation();
+  const [t] = useTranslation();
 
-    return (
-        <>
-            <UIPageTitle title={t("Teams List")}/>
-            <UIContent>
-                <LoaderWrapper loading={isLoading}>
-                    <UIColumn wide>
-                        <AccessManager staticPermission={Permissions.createTeams}>
-                            <UIButton
-                                title={t("Add Team")}
-                                onClick={() => handleRedirect("new")}
-                                center
-                                primary
-                            />
-                        </AccessManager>
-                    </UIColumn>
+  return (
+    <>
+      <UIPageTitle title={t("Teams List")}/>
+      <UIContent>
+        <LoaderWrapper loading={isLoading}>
+          <UIColumn wide>
+            <AccessManager staticPermission={Permissions.createTeams}>
+              <UIButton
+                title={t("Add Team")}
+                onClick={() => handleRedirect("new")}
+                center
+                primary
+              />
+            </AccessManager>
+          </UIColumn>
 
-                    {(teams || []).map(team => {
-                        const match = result
-                            .teams
-                            .map(t => t.id)
-                            .includes(team.id);
-                        return <UIColumn key={team.id}>
-                            <UICard searched={match}>
-                                <UICardBlock>
-                                    <h3>{team.name}</h3>
-                                    <span style={
-                                        {
-                                            fontSize: '0.8rem',
-                                            display: 'inline-flex',
-                                            alignItems: 'center'
-                                        }
-                                    }>{match && t('Matches searched query')}</span>
-                                </UICardBlock>
-                                <UICardBlock>
-                                    <Icon name="users"/>{team.membersAmount} Member(s)
-                                </UICardBlock>
-                                <AccessManager staticPermission={Permissions.manageTeams}>
-                                    <UICardBlock>
-                                        <UIButton title={t("Manage")} onClick={() => handleRedirect(team.id)}/>
-                                        <UIButton title={t("Delete")} secondary loading={team.deleteLoading}
-                                                  disabled={team.deleteLoading} onClick={() => deleteTeam(team.id)}/>
-                                    </UICardBlock>
-                                </AccessManager>
-                            </UICard>
-                        </UIColumn>;
-                    })}
-                </LoaderWrapper>
-            </UIContent>
-        </>
-    );
+          {teams &&
+          (teams.length > 0
+              ? teams.map(team => {
+                const match = result
+                  .teams
+                  .map(t => t.id)
+                  .includes(team.id);
+                return <UIColumn key={team.id}>
+                  <UICard searched={match}>
+                    <UICardBlock>
+                      <h3>{team.name}</h3>
+                      <span style={
+                        {
+                          fontSize: '0.8rem',
+                          display: 'inline-flex',
+                          alignItems: 'center'
+                        }
+                      }>{match && t('Matches searched query')}</span>
+                    </UICardBlock>
+                    <UICardBlock>
+                      <Icon name="users"/>{team.membersAmount} Member(s)
+                    </UICardBlock>
+                    <AccessManager staticPermission={Permissions.manageTeams}>
+                      <UICardBlock>
+                        <UIButton title={t("Manage")} onClick={() => handleRedirect(team.id)}/>
+                        <UIButton title={t("Delete")} secondary loading={team.deleteLoading}
+                                  disabled={team.deleteLoading} onClick={() => deleteTeam(team.id)}/>
+                      </UICardBlock>
+                    </AccessManager>
+                  </UICard>
+                </UIColumn>;
+              })
+              : <div className={styles.noItemsLabel}>{t("No items")}</div>
+          )}
+        </LoaderWrapper>
+      </UIContent>
+    </>
+  );
 };
 
-const mapState = (state: IAppState) => {
-    return {
-        teams: state.teams.teams,
-        companyUsers: state.teams.companyUsers,
-        isLoading: state.teams.isLoading,
-        result: state.search.result
-    };
-};
+const mapState = (state: IAppState) => ({
+  teams: state.teams.teams,
+  companyUsers: state.teams.companyUsers,
+  isLoading: state.teams.isLoading,
+  result: state.search.result
+});
 
 const mapDispatch = {
-    loadTeams: loadTeamsRoutine,
-    loadUsers: loadCompanyUsersRoutine,
-    deleteTeam: deleteTeamRoutine,
-    clearCurrentTeam: clearCurrentTeamRoutine
+  loadTeams: loadTeamsRoutine,
+  loadUsers: loadCompanyUsersRoutine,
+  deleteTeam: deleteTeamRoutine,
+  clearCurrentTeam: clearCurrentTeamRoutine
 };
 
 const connector = connect(mapState, mapDispatch);
