5604a06d81c4fb08d0209beaead0425de4a158b6
Ivan Husarov <ivan.husarov.mp.2018@lpnu.ua>
2020-09-04 17:17:07 +0300

    move sections to form editor

--- frontend/src/reducers/formEditor/reducer.ts
+++ frontend/src/reducers/formEditor/reducer.ts
@@ -1,7 +1,16 @@
-import {ISectionsState} from "../section/reducer";
-import {IQuestionnaire} from "../../models/forms/Questionnaires/types";
-import {IAppState} from "../../models/IAppState";
-import {IQuestion} from "../../models/forms/Questions/IQuesion";
+import { ISectionsState } from "../section/reducer";
+import { IQuestionnaire } from "../../models/forms/Questionnaires/types";
+import { IAppState } from "../../models/IAppState";
+import { IQuestion } from "../../models/forms/Questions/IQuesion";
+import {
+    setCurrentSectionRoutine,
+    createSectionRoutine,
+    loadSectionsByQuestionnaireRoutine,
+    setCurrentSectionIdRoutine,
+    updateSectionsRoutine,
+    deleteQuestionFromSectionRoutine, loadSavedSectionsByQuestionnaireRoutine
+} from "sagas/sections/routines";
+import { loadQuestionsBySectionRoutine } from "sagas/questions/routines";
 
 const init = {
     questionnaire: {} as IQuestionnaire,
@@ -10,8 +19,70 @@ const init = {
     isLoading: false
 };
 
-const formEditorReducer = (state: IAppState["formEditor"] = init, {type, payload}) => {
+const formEditorReducer = (state: IAppState["formEditor"] = init, { type, payload }) => {
     switch (type) {
+        case setCurrentSectionRoutine.TRIGGER:
+        case createSectionRoutine.SUCCESS:
+        case deleteQuestionFromSectionRoutine.SUCCESS:
+            return {
+                ...state,
+                sections: {
+                    ...state.sections,
+                    current: payload
+                }
+            };
+        case loadSectionsByQuestionnaireRoutine.TRIGGER:
+        case loadSavedSectionsByQuestionnaireRoutine.TRIGGER:
+        case loadQuestionsBySectionRoutine.TRIGGER:
+            return {
+                ...state,
+                isLoading: true,
+                sections: {
+                    ...state.sections,
+                    questionnaireId: payload
+                }
+            };
+        case loadSectionsByQuestionnaireRoutine.SUCCESS:
+        case loadSavedSectionsByQuestionnaireRoutine.SUCCESS:
+        case updateSectionsRoutine.SUCCESS:
+            return {
+                ...state,
+                sections: {
+                    ...state.sections,
+                    list: payload,
+                    current: payload[0]
+                }
+            };
+        case loadSectionsByQuestionnaireRoutine.FAILURE:
+        case loadSavedSectionsByQuestionnaireRoutine.FAILURE:
+        case loadQuestionsBySectionRoutine.FAILURE:
+            return {
+                ...state,
+                isLoading: false
+            };
+        case loadQuestionsBySectionRoutine.SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                sections: {
+                    ...state.sections,
+                    current: {
+                        ...state.sections.current,
+                        questions: payload
+                    }
+                }
+            };
+        case setCurrentSectionIdRoutine.SUCCESS:
+            return {
+                ...state,
+                sections: {
+                    ...state.sections,
+                    current: {
+                        ...state.sections.current,
+                        id: payload
+                    }
+                }
+            };
         default:
             return state;
     }
