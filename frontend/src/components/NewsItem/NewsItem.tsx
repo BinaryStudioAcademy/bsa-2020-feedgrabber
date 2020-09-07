e567e9daa5273ddb0baa1c4a3637bd6789e6664c
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-09-07 10:54:35 +0300

    fix styles

--- frontend/src/components/NewsItem/NewsItem.tsx
+++ frontend/src/components/NewsItem/NewsItem.tsx
@@ -11,20 +11,23 @@ const NewsItem: FC<INewsItemProps> = ({item}) => {
     return (
         <UICardBlock key={item.id}
                      className={styles.newsItemContainer}>
+            <>
             {item.image
-                ? <img src={item.image.link} alt='' height="240" width="180"/>
+                ? <img src={item.image?.link} alt='' height="240" width="180"/>
                 : <div/>
-            } {/* scale 4:3 * 60*/}
+            } {/* scale 4:3 * 60 */}
             <div className={styles.detailesContainer}>
-                <div className={styles.type}>{item.type}</div>
-                <div className={styles.title}>{item.title}</div>
-                <div className={styles.body}>{item.body}</div>
-                <div className={item.image ? styles.authorContainer : styles.authorContainerRight}>
+                <div className={styles.mainContainer}>
+                    <div className={styles.type}>{item.type}</div>
+                    <div className={styles.title}>{item.title}</div>
+                    <div className={styles.body}>{item.body}</div>
+                </div>
+                <div className={styles.authorContainer}>
                     {item.user.avatar
                         ? <img src={item.user.avatar} alt='avatar' height="50" width="50"/>
-                        : null
+                        : <div/>
                     }
-                    <div className={styles.detailesContainer}>
+                    <div className={styles.detailesAuthorContainer}>
                         <div className={styles.userName}>{item.user.username}</div>
                         <div className={styles.date}>{item.createdAt}</div>
                     </div>
@@ -33,6 +36,7 @@ const NewsItem: FC<INewsItemProps> = ({item}) => {
             <div className={styles.reactionsContainer}>
 
             </div>
+            </>
         </UICardBlock>
     );
 };
