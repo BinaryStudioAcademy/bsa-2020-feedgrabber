6a628aef4dae873075a216dcaeab79626174c7e2
NataNorth <49091101+NataNorth@users.noreply.github.com>
2020-09-08 23:21:27 +0300

    add modal

--- frontend/src/components/UI/UISectionCard/index.tsx
+++ frontend/src/components/UI/UISectionCard/index.tsx
@@ -2,7 +2,8 @@ import React, { FC, useState } from "react";
 import styles from "./styles.module.sass";
 import {ISection} from "../../../reducers/formEditor/reducer";
 import { useTranslation } from "react-i18next";
-import { Icon } from "semantic-ui-react";
+import { Icon, Modal } from "semantic-ui-react";
+import UIButton from "../UIButton";
 
 export interface IUISectionProps {
     section: ISection;
@@ -22,12 +23,35 @@ const UISection: FC<IUISectionProps> = ({
     const [t] = useTranslation();
     const [title, setTitle] = useState(section.title);
     const [description, setDescription] = useState(section.description);
+    const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+
+    const confirmationModal = () => {
+        return (<Modal
+            size="tiny"
+            open={showConfirmationModal}
+            closeOnDimmerClick
+            onClose={() => setShowConfirmationModal(false)}
+        >
+            <Modal.Header>
+                {t("You can't revert this action. ")}
+                {t("Are you sure you want to delete this section?")}
+            </Modal.Header>
+            <Modal.Actions>
+                <UIButton title={t("Yes")}
+                        onClick={() => {
+                            setShowConfirmationModal(false);
+                            onDelete({sectionId: section.id, questionnaireId: questionnaireId});
+                        }}/>
+                <UIButton title={t("No")} submit onClick={() => setShowConfirmationModal(false)}/>
+            </Modal.Actions>
+        </Modal>);
+    };
 
     return (
         <div className={styles.headerContainer}>
                 <div className={["ui very padded segment", styles.sectionCard].join(' ')}>
                     {!main ? <div className={styles.buttonContainer}>
-                        <button onClick={() => onDelete({sectionId: section.id, questionnaireId: questionnaireId})}>
+                        <button onClick={() => setShowConfirmationModal(true)}>
                             <Icon name='delete'/>
                         </button>
                     </div> : null}
@@ -44,6 +68,7 @@ const UISection: FC<IUISectionProps> = ({
                     onBlur={e => {
                         onChanged(section.id, title, description);}}/>
                 </div>
+                {confirmationModal()}
         </div>
         );
 };
