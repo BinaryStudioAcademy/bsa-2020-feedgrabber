0501885ea01582aaf647b15be0bda16bc85c7f09
Ivan Husarov <ivan.husarov.mp.2018@lpnu.ua>
2020-09-10 15:15:03 +0300

    fixed dont know why optional not working

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -77,7 +77,8 @@ const Routing: FC<RoutingProps> = ({isLoading, getUser}) => {
                     <PrivateRoute exact path="/questionnaires/:id/requests" component={RequestsPage}/>
                     <PrivateRoute exact path="/report/:id" component={ReportPage}/>
                     <PrivateRoute exact path="/report/:id/:respondent/:username" component={RespondentReport}/>
-                    <PrivateRoute path="/response/:id/modify?/:responseId?" component={QuestionnaireResponse}/>
+                    <PrivateRoute exact path="/response/:id" component={QuestionnaireResponse}/>
+                    <PrivateRoute exact path="/response/:id/modify/:responseId" component={QuestionnaireResponse}/>
                     <PrivateRoute exact path="/questions" component={QuestionsList}/>
                     <PrivateRoute exact path="/employees" component={UserList}/>
                     <PrivateRoute exact path="/question/:id" component={QuestionDetailsContainer}/>
